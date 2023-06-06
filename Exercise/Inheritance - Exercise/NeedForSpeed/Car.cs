1bb928996e97e962136e72f98f7f1adcf1f8ed15
MustyX23 <mustyx900@gmail.com>
2023-06-06 23:22:48 +0300

    NeedForSpeed\Car.cs

---
+++ Exercise/Inheritance - Exercise/NeedForSpeed/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public const double DefaultFuelConsumption = 3;
+        public override double FuelConsumption { get; set; } = DefaultFuelConsumption;
+
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }       
+    }
+}
