be1974ac4a81c09b53a719559d040d70db868665
MustyX23 <mustyx900@gmail.com>
2023-06-07 21:56:24 +0300

    NeedForSpeed\Vehicle.cs

---
+++ Exercise/Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {       
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuelConsumption { get; set; }
+        public const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+        }
+
+    }
+}
