e2a86dba74859a52c55a8160aee06005d5946332
MustyX23 <mustyx900@gmail.com>
2023-06-08 22:56:29 +0300

    ShoppingSpree\Person.cs

---
+++ Exercise/Encapsulation - Excercise/ShoppingSpree/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+    }
+}
