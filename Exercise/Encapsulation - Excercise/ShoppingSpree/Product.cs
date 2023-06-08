19b3c4a5b026d1e51be0d99db902d93fa660c683
MustyX23 <mustyx900@gmail.com>
2023-06-08 22:56:58 +0300

    ShoppingSpree\Product.cs

---
+++ Exercise/Encapsulation - Excercise/ShoppingSpree/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Cost
+        {
+            get { return cost; }
+            private set
+            {
+                cost = value;
+            }
+        }
+    }
+}
