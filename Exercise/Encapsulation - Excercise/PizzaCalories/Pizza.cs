6a9b144514394e342701101f2907997fb08b0fae
MustyX23 <mustyx900@gmail.com>
2023-06-08 22:55:45 +0300

    PizzaCalories\Pizza.cs

---
+++ Exercise/Encapsulation - Excercise/PizzaCalories/Pizza.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Pizza
+    {
+        private string name;
+        private string dough;
+        private string toppings;
+
+        public Pizza(string name, string dough, string toppings)
+        {
+            Name = name;
+            Dough = dough;
+            Toppings = toppings;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set => name = value;
+        }
+        public string Dough
+        {
+            get => dough;
+            set => dough = value;
+        }
+        public string Toppings
+        {
+            get => toppings;
+            set => toppings = value;
+        }
+    }
+}
