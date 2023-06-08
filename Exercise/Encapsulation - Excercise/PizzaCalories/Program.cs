726ef915f81bb19a2a5ca02ebb114df96eee4d23
MustyX23 <mustyx900@gmail.com>
2023-06-08 22:56:12 +0300

    PizzaCalories\Program.cs

---
+++ Exercise/Encapsulation - Excercise/PizzaCalories/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Dough White Chewy 100
+            try
+            {
+                while (true)
+                {
+                    string[] input = Console.ReadLine().Split(' ');
+                    string flourType = input[1];
+                    string bakingTechnique = input[2];
+                    double grams = double.Parse(input[3]);
+
+                    Dough dough = new Dough(flourType, bakingTechnique, grams);
+                    Console.WriteLine(dough.Calories);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+    }
+}
