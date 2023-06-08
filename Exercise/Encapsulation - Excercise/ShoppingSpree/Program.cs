7ed653c67398e634854b9dc855e3d1d28df5b3ab
MustyX23 <mustyx900@gmail.com>
2023-06-08 22:57:03 +0300

    ShoppingSpree\Program.cs

---
+++ Exercise/Encapsulation - Excercise/ShoppingSpree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                //Peter=11;George=4
+                string[] people = input.Split(';');
+                string name = people[0];
+                string age = people[1];
+            }
+        }
+    }
+}
