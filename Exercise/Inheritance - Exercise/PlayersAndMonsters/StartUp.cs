5be125321febd46d04a8344ab101f1249341a88d
MustyX23 <mustyx900@gmail.com>
2023-06-09 11:03:02 +0300

    StartUp.cs

---
+++ Exercise/Inheritance - Exercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Wizard wizard = new Wizard("SashoMagiosnika", 17);
+            System.Console.WriteLine(wizard.Level);
+        }
+    }
+}
