79253a2b06d7ae2c265b36479cf5e4f25ea9cb9a
MustyX23 <mustyx900@gmail.com>
2023-06-09 11:03:10 +0300

    Cake.cs

---
+++ Exercise/Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal CakePrice = 5;
+        private const double Calories = 1000;
+        private const double Grams = 250;
+
+        public Cake(string name)
+            : base(name, CakePrice, Grams, Calories)
+        {
+        }
+    }
+}
