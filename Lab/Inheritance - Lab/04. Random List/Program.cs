9ba3440fca9e676a1ac93882aa9403abce0821ba
MustyX23 <mustyx900@gmail.com>
2023-06-11 00:09:33 +0300

    04. Random List\Program.cs

---
+++ Lab/Inheritance - Lab/04. Random List/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings = new RandomList() { "banana", "apple", "grape"};
+            
+            Console.WriteLine(strings.RandomString());
+        }
+    }
+}
