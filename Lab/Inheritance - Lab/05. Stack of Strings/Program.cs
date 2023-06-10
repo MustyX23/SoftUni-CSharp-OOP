5593d62d671899f5c30043c7d2619037d61206b0
MustyX23 <mustyx900@gmail.com>
2023-06-11 00:09:18 +0300

    05. Stack of Strings\Program.cs

---
+++ Lab/Inheritance - Lab/05. Stack of Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            stack.Push("Apple");
+            stack.Push("Banana");
+            stack.Push("Orange");
+
+            bool isEmpty = stack.IsEmpty();
+            Console.WriteLine("Is stack empty? " + isEmpty);
+
+            stack.AddRange(new[] { "Grapes", "Mango" });
+
+            string topItem = stack.Peek();
+            Console.WriteLine("Top item in stack: " + topItem);
+        }
+    }
+}
