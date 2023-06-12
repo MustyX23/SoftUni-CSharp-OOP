40592f4cfff9bdcc7f604d958640809050493293
MustyX23 <mustyx900@gmail.com>
2023-06-12 22:06:51 +0300

    StartUp.cs

---
+++ Exercise/Inheritance - Exercise/Zoo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //...
+            Lizard gushter = new Lizard("Hameleoncho");
+            System.Console.WriteLine(gushter.Name);
+        }
+    }
+}
