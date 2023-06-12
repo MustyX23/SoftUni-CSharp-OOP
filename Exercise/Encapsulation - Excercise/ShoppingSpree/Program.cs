2f00a4786f68a7f268f820e9f8e2d731023079ad
MustyX23 <mustyx900@gmail.com>
2023-06-12 22:06:20 +0300

    Program.cs

--- Exercise/Encapsulation - Excercise/ShoppingSpree/Program.cs
+++ Exercise/Encapsulation - Excercise/ShoppingSpree/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingSpree
 {
@@ -6,15 +8,60 @@ namespace ShoppingSpree
     {
         static void Main(string[] args)
         {
-            string input;
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
 
-            while ((input = Console.ReadLine()) != "END")
+
+            try
             {
-                //Peter=11;George=4
-                string[] people = input.Split(';');
-                string name = people[0];
-                string age = people[1];
+                string[] nameMoneyPairs = Console.ReadLine()
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var nameMoneyPair in nameMoneyPairs)
+                {
+                    string[] nameMoney = nameMoneyPair
+                        .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                    Person person = new Person(nameMoney[0], decimal.Parse(nameMoney[1]));
+
+                    people.Add(person);
+                }
+
+                string[] productCostPairs = Console.ReadLine()
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var productCostPair in productCostPairs)
+                {
+                    string[] productCost = productCostPair
+                        .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                    Product product = new Product(productCost[0], decimal.Parse(productCost[1]));
+
+                    products.Add(product);
+                }
+                string input;
+
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] personProductPair = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string personName = personProductPair[0];
+                    string productName = personProductPair[1];
+
+                    Person person = people.FirstOrDefault(person => person.Name == personName);
+                    Product product = products.FirstOrDefault(product => product.Name == productName);
+
+                    if (person != null && product != null)
+                    {
+                        Console.WriteLine(person.Add(product));
+                    }
+                }
+
+                Console.WriteLine(String.Join(Environment.NewLine, people));
             }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }          
         }
     }
 }
