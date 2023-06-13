ef9d29c682ed4e75bcd57505f16c84a094ec48a3
MustyX23 <mustyx900@gmail.com>
2023-06-14 00:23:54 +0300

    Person.cs

---
+++ Lab/Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{firstName} {lastName} receives {Salary:f2} leva.";
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increase = Salary * percentage / 100;
+            if (age < 30)
+            {
+                increase /= 2; 
+            }
+
+            Salary += increase;
+        }
+    }
+}
