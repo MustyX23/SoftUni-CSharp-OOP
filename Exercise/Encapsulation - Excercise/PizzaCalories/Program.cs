d89fef7520b0e0cac2376a380911203fa3f590d0
MustyX23 <mustyx900@gmail.com>
2023-06-14 00:23:35 +0300

    Update Program.cs

--- Exercise/Encapsulation - Excercise/PizzaCalories/Program.cs
+++ Exercise/Encapsulation - Excercise/PizzaCalories/Program.cs
@@ -6,26 +6,69 @@ namespace PizzaCalories
     {
         static void Main(string[] args)
         {
-            //Dough White Chewy 100
-            try
-            {
-                while (true)
-                {
-                    string[] input = Console.ReadLine().Split(' ');
-                    string flourType = input[1];
-                    string bakingTechnique = input[2];
-                    double grams = double.Parse(input[3]);
+            Pizza pizza = new Pizza();
 
-                    Dough dough = new Dough(flourType, bakingTechnique, grams);
-                    Console.WriteLine(dough.Calories);
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] pizzaInfo = input.Split(" ");
+
+                string item = pizzaInfo[0];
+
+                if (item == "Pizza")
+                {
+                    try
+                    {
+                        pizza.Name = item;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (item == "Dough")
+                {
+                    Dough dough = new Dough();
+                    
+                    try
+                    {
+                        dough.Type = pizzaInfo[1];
+                        dough.BakingTechnique = pizzaInfo[2];
+                        dough.Weight = int.Parse(pizzaInfo[3]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    pizza.Dough = dough;
+                }
+                else if (item == "Topping")
+                {
+                    Topping topping = new Topping();
+
+                    try
+                    {
+                        topping.Type = pizzaInfo[1];
+                        topping.Weight = int.Parse(pizzaInfo[2]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    pizza.AddTopping(topping);
                 }
             }
-            catch (Exception ex)
+
+            if (pizza.ToppingsCount() > 10)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Number of toppings should be in range [0..10].");
+                return;
             }
-            
-            
+
+            Console.WriteLine(pizza);
         }
     }
 }
