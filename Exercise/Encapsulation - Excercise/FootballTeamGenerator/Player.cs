adc9a1bca75908c6d66a0071187337a24b2f0743
MustyX23 <mustyx900@gmail.com>
2023-06-14 00:23:17 +0300

    Player.cs

---
+++ Exercise/Encapsulation - Excercise/FootballTeamGenerator/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            Name = name;
+            Endurance = endurance;
+            Sprint = sprint;
+            Dribble = dribble;
+            Passing = passing;
+            Shooting = shooting;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+        public int Endurance 
+        {
+            get { return endurance; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException($"Endurance should be between 0 and 100.");
+                }
+                endurance = value;
+            }
+        }
+        public int Sprint
+        {
+            get { return sprint; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException($"Sprint should be between 0 and 100.");
+                }
+                sprint = value;
+            }
+        }
+        public int Dribble
+        {
+            get { return dribble; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException($"Dribble should be between 0 and 100.");
+                }
+                dribble = value;
+            }
+        }
+        public int Passing
+        {
+            get { return passing; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException($"Passing should be between 0 and 100.");
+                }
+                passing = value;
+            }
+        }
+        public int Shooting
+        {
+            get { return shooting; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException($"Shooting should be between 0 and 100.");
+                }
+                passing = value;
+            }
+        }
+
+        public double OverallRating()
+        {
+            double rating = (Endurance + Sprint + Dribble + Passing + Shooting / 5);
+            return rating;
+        }
+
+
+
+    }
+}
