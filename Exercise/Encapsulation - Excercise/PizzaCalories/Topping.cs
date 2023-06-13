16fc991c234c5d8bd85c1acdcdff0ee492b13c11
MustyX23 <mustyx900@gmail.com>
2023-06-14 00:23:43 +0300

    Topping.cs

---
+++ Exercise/Encapsulation - Excercise/PizzaCalories/Topping.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private double weight;
+        private string type;
+        private double totalCals;
+
+        public double Weight
+        {
+            set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new InvalidOperationException($"{type} weight should be in the range [1..50].");
+                }
+                weight = value;
+            }
+        }
+        public string Type
+        {
+            set
+            {
+                if (value.ToLower() == "meat" || value.ToLower() == "veggies" ||
+                value.ToLower() == "cheese" || value.ToLower() == "sauce")
+                {
+                    type = value;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Cannot place {value} on top of your pizza.");
+                }
+            }
+        }
+        public double TotalCals()
+        {
+
+            totalCals = weight * 2;
+
+            if (type == "meat")
+            {
+                totalCals *= 2;
+            }
+            else if (type == "veggies")
+            {
+                totalCals *= 0.8;
+            }
+            else if (type == "cheese")
+            {
+                totalCals *= 1.1;
+            }
+            else if (type == "sauce")
+            {
+                totalCals *= 0.9;
+            }
+            return totalCals;
+        }
+    }
+}
