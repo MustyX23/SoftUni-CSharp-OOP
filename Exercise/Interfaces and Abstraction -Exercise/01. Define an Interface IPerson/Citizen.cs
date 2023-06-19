d1d8afbc77a2ca34aa50b2aa4288f77ea95a6afe
MustyX23 <mustyx900@gmail.com>
2023-06-20 01:06:51 +0300

    Citizen.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonInfo
+{
+    public partial class StartUp
+    {
+        public class Citizen : IPerson
+        {
+            public Citizen(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+        }
+    }
+}
