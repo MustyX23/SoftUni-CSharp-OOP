43ad3ae521eb1d79232b662fd0759c065a953ab3
MustyX23 <mustyx900@gmail.com>
2023-06-21 00:04:56 +0300

    Program.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/03. Telephony/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //0882134215 0882134333 0899213421 0558123 3333123
+            string[] phoneNumbers = Console.ReadLine().Split(' ');
+            //http://softuni.bg http://youtube.com http://www.g00gle.com
+            string[]urls = Console.ReadLine().Split(' ');
+
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                ICall smartPhone = new Smartphone();
+                smartPhone.Call(phoneNumber);
+                ICall stationaryPhone = new StationaryPhone();
+                stationaryPhone.Call(phoneNumber);
+            }
+
+            foreach (string url in urls)
+            {
+                IBrowse smartPhone = new Smartphone();
+                smartPhone.Browse(url);
+
+            }
+
+
+        }
+    }
+}
