ef5af6210f657c02e20d0cda43b4c9ffc45bca78
MustyX23 <mustyx900@gmail.com>
2023-06-21 00:05:17 +0300

    Citizen.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/04. Border Control/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string iD)
+        {
+            Name = name;
+            Age = age;
+            ID = iD;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+
+    }
+}
