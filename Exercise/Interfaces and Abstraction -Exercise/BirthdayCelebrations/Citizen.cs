fed0b7175303ff1a06799591535de8465ad102a6
MustyX23 <mustyx900@gmail.com>
2023-06-21 00:05:41 +0300

    Citizen.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,23 @@
+﻿using BirthdayCelebrations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string iD, string birthDate)
+        {
+            Name = name;
+            Age = age;
+            ID = iD;
+            BirthDate = birthDate;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
