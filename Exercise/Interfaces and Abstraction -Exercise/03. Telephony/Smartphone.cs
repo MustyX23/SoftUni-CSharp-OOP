7df4d7219e9b30595c3d7a4ebfefcc4a36daf259
MustyX23 <mustyx900@gmail.com>
2023-06-21 00:05:00 +0300

    Smartphone.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/03. Telephony/Smartphone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICall, IBrowse
+    {
+        public void Call(string phoneNumber)
+        {
+            if (!phoneNumber.All(p => Char.IsDigit(p)))
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            else if (phoneNumber.Length == 10)
+            {
+                Console.WriteLine($"Calling... {phoneNumber}");
+            }
+        }
+        public void Browse(string url)
+        {
+            if (!url.All(u => Char.IsDigit(u)))
+            {
+                Console.WriteLine("Invalid URL!");
+                return;
+            }
+
+            Console.WriteLine($"Browsing: {url}!");
+        }
+
+    }
+}
