422cd77b8c974331bc6d54dc633891c34ceef22a
MustyX23 <mustyx900@gmail.com>
2023-06-21 00:05:03 +0300

    StationaryPhone.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/03. Telephony/StationaryPhone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class StationaryPhone : ICall
+    {
+        public void Call(string phoneNumber)
+        {
+            if (!phoneNumber.All(p => Char.IsDigit(p)))
+            {
+                Console.WriteLine("Invalid number!");
+                return;
+            }
+            if (phoneNumber.Length == 7)
+            {
+                Console.WriteLine($"Dialing... {phoneNumber}");
+            }
+        }
+    }
+}
