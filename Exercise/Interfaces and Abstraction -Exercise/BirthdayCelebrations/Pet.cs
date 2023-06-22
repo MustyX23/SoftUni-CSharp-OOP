c84bf59612cade9cce01fa3fcac4997182012d35
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:04:22 +0300

    Pet.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/BirthdayCelebrations/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    public class Pet : IBirthable
+    {
+        public Pet(string name, string birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
