3b9b68d0ba8f2c079a1154eaeceedb24b1150e0f
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:04:26 +0300

    Program.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/BirthdayCelebrations/Program.cs
@@ -0,0 +1,55 @@
+﻿using BorderControl;
+using System;
+using System.Collections.Generic;
+
+namespace BirthdayCelebrations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IBirthable> citizensAndPets = new List<IBirthable>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+
+                string[] commandArgs = input.Split(' ');
+
+                string name = commandArgs[1];
+
+                if (commandArgs.Length == 5)
+                {
+                    //Citizen                    
+                    int age = int.Parse(commandArgs[2]);
+                    string id = commandArgs[3];
+                    string dateOfBirth = commandArgs[4];
+                    citizensAndPets.Add(new Citizen(name, age, id, dateOfBirth));
+                }
+                else if (commandArgs.Length == 3 && commandArgs[0] == "Pet")
+                {
+                    //Pet
+                    string date = commandArgs[2];
+                    citizensAndPets.Add(new Pet(name, date));
+                }
+                else if (commandArgs.Length == 3 && commandArgs[0] == "Robot")
+                {
+                    continue;
+                }
+            }
+
+            string yearToSearchUpon = Console.ReadLine();
+
+            foreach (var type in citizensAndPets)
+            {
+                string birthYear = type.BirthDate.Substring(type.BirthDate.Length - 4);
+
+                if (yearToSearchUpon == birthYear)
+                {
+                    Console.WriteLine(type.BirthDate);
+                }
+            }
+        }
+
+    }
+}
