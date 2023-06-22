00028cda29d52907ed124070e370780559ccde06
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:05:06 +0300

    Rebel.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/FoodShortage/Rebel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage
+{
+    public class Rebel : Human
+    {
+        public Rebel(string name, int age, string group)
+            : base(name, age)
+        {
+            Group = group;
+            Food = 0;
+            Type = "Rebel";
+        }
+
+        public string Group { get; set; }
+        public override int Food { get; set; }
+
+        public override void BuyFood()
+        {
+            Food += 5;
+        }
+    }
+}
