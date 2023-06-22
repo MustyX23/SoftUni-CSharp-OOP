3e0a97d6c0d11f7f61d0c7371aaba4a2e03929e9
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:04:31 +0300

    Citizen.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/FoodShortage/Citizen.cs
@@ -0,0 +1,29 @@
+﻿using BirthdayCelebrations;
+using FoodShortage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : Human, IIdentifiable, IBirthable
+    {
+
+        public Citizen(string name, int age, string id, string birthDate)
+            : base(name, age)
+        {
+            ID = id;
+            BirthDate = birthDate;
+            Food = 0;
+            Type = "Citizen";
+        }
+        public string ID { get; set; }
+        public string BirthDate { get; set; }
+        public override int Food { get; set; }
+
+        public override void BuyFood()
+        {
+            Food += 10;
+        }
+    }
+}
