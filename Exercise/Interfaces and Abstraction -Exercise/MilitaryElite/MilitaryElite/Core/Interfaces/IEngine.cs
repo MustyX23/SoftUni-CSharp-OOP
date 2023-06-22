e1455da88fc7c50eacc59f5ab8ed2c68c88f1d74
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:05:19 +0300

    IEngine.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Core/Interfaces/IEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Core.Interfaces
+{
+    public interface IEngine
+    {
+        void Run();
+    }
+}
