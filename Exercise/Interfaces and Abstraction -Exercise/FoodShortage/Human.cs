3b89b53b20beac0ebbf4e73647d31ce825bb6cb3
MustyX23 <mustyx900@gmail.com>
2023-06-22 19:04:44 +0300

    Human.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/FoodShortage/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage
+{
+    public abstract class Human : IBuyer
+    {
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Type = string.Empty;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; }
+        public string Type { get; set; }
+
+        public abstract int Food { get; set; }
+        
+        public abstract void BuyFood();
+        
+    }
+}
