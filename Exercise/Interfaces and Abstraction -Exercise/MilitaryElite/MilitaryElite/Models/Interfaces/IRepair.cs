4da5546bee9825a27f2fba850fd43c8b408161df
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:11 +0300

    IRepair.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Interfaces/IRepair.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface IRepair
+    {
+        string PartName { get; }
+        int HoursWorked { get; }
+    }
+}
