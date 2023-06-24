74979e8b9b3d4a0cc0d2c0913d6774cdb443ebe4
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:38 +0300

    Soldier.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,28 @@
+﻿using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public abstract class Soldier : ISoldier
+    {
+        protected Soldier(string id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName} Id: {Id}";
+        }
+    }
+}
