ba576131d5edd340a6884d5197fd477492098667
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:56:05 +0300

    Program.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Program.cs
@@ -0,0 +1,17 @@
+﻿using MilitaryElite.Core;
+using MilitaryElite.Core.Interfaces;
+using MilitaryElite.Models;
+using MilitaryElite.Models.Interfaces;
+using System;
+
+namespace MilitaryElite
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
