b7ee68ddd9826be01e8312f47648e124e865099e
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:00 +0300

    ILieutenantGeneral.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Interfaces/ILieutenantGeneral.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface ILieutenantGeneral : IPrivate
+    {
+        IReadOnlyCollection<IPrivate> Privates { get; }
+    }
+}
