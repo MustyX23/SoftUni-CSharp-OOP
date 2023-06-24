10d60ef58cb0502ba44920c3b01bdf1173e6999b
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:57 +0300

    Spy.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Spy.cs
@@ -0,0 +1,21 @@
+﻿using MilitaryElite.Enums;
+using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Spy : Soldier, ISpy
+    {
+        public Spy(string id, string firstName, string lastName, int codeNumber)
+            : base(id, firstName, lastName)
+        {
+            CodeNumber = codeNumber;
+        }
+
+        public int CodeNumber { get; private set; }
+        public override string ToString()
+            => base.ToString() + $"{Environment.NewLine}Code Number: {CodeNumber}";
+    }
+}
