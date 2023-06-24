02756b89188befc66d8c129bda688e12bfaae9fc
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:25 +0300

    LieutenantGeneral.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/LieutenantGeneral.cs
@@ -0,0 +1,34 @@
+﻿using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class LieutenantGeneral : Private, ILieutenantGeneral
+    {
+        public LieutenantGeneral(string id, string firstName, string lastName
+            , decimal salary, IReadOnlyCollection<IPrivate> privates)
+            : base(id, firstName, lastName, salary)
+        {
+            Privates = privates;
+        }
+
+        public IReadOnlyCollection<IPrivate> Privates { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(base.ToString());
+            result.AppendLine("Privates: ");
+
+            foreach (var @private in Privates)
+            {
+                result.AppendLine(@private.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
