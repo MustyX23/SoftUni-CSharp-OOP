9fc970665df4c0d2ab19578177ad519d31527252
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:14 +0300

    ISoldier.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Interfaces/ISoldier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface ISoldier
+    {
+        //ISoldier should hold id, first name, and last name
+        string Id { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
