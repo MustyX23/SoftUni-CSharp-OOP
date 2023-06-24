c1f73b371bd8ca4454ddb2367eaab093ccfd4b2d
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:54:57 +0300

    IEngineer.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Interfaces/IEngineer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface IEngineer : ISpecialisedSoldier
+    {
+        IReadOnlyCollection<IRepair> Repairs { get; }
+    }
+}
