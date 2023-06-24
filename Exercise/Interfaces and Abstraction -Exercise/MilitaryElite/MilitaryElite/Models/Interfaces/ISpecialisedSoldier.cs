c6592ca1c967fd6603a81c28c87773d273230ed3
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:18 +0300

    ISpecialisedSoldier.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Interfaces/ISpecialisedSoldier.cs
@@ -0,0 +1,12 @@
+﻿using MilitaryElite.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface ISpecialisedSoldier : IPrivate
+    {
+        Corps Corps { get;}
+    }
+}
