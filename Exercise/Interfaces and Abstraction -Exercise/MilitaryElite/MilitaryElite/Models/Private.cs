84788761e99314a106f50036d883651b0d84651f
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:55:31 +0300

    Private.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Private.cs
@@ -0,0 +1,23 @@
+﻿using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Private : Soldier, IPrivate
+    {
+        public Private(string id, string firstName, string lastName, decimal salary)
+            : base(id, firstName, lastName)
+        {
+            Salary = salary;
+        }
+
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Salary: {Salary:f2}";
+        }
+    }
+}
