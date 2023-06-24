de4ef6197a766d8e4a9b3bffc6e8272ba20acdc7
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:54:46 +0300

    Commando.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Commando.cs
@@ -0,0 +1,33 @@
+﻿using MilitaryElite.Enums;
+using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Commando : SpecialisedSoldier ,ICommando
+    {
+        public Commando(string id, string firstName, string lastName,
+            decimal salary, Corps corps, IReadOnlyCollection<IMission> missions)
+            : base(id, firstName, lastName, salary, corps)
+        {
+            Missions = missions;
+        }
+
+        public IReadOnlyCollection<IMission> Missions { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(base.ToString());
+            result.AppendLine("Missions: ");
+            foreach (var mission in Missions)
+            {
+                result.AppendLine(mission.ToString());
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
