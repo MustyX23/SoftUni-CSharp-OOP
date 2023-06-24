3cf4e1403554dac7e7c03e6b55d8039eab7b4ed5
MustyX23 <mustyx900@gmail.com>
2023-06-24 14:54:50 +0300

    Engineer.cs

---
+++ Exercise/Interfaces and Abstraction -Exercise/MilitaryElite/MilitaryElite/Models/Engineer.cs
@@ -0,0 +1,33 @@
+﻿using MilitaryElite.Enums;
+using MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Engineer : SpecialisedSoldier, IEngineer
+    {
+        public Engineer(string id, string firstName, string lastName, 
+            decimal salary, Corps corps, IReadOnlyCollection<IRepair> repairs)
+            : base(id, firstName, lastName, salary, corps)
+        {
+            Repairs = repairs;
+        }
+
+        public IReadOnlyCollection<IRepair> Repairs { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(base.ToString());
+            result.AppendLine("Repairs:");
+            foreach (var repair in Repairs)
+            {
+                result.AppendLine(repair.ToString());
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
