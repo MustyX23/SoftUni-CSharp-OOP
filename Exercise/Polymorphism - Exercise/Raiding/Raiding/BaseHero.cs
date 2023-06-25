3548dce61156f0981a9ee0f61b66572690a31168
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:17:53 +0300

    BaseHero.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/Raiding/BaseHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        protected BaseHero(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public int Power { get; set; }
+
+        public abstract void CastAbility();
+    }
+}
