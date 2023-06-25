51a165088e450e4b2e5ef3b34adafd2a2d66046b
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:17:58 +0300

    Druid.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/Raiding/Druid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        public Druid(string name)
+            : base(name)
+        {
+            Power = 80;
+        }
+
+        public override void CastAbility()
+        {
+            Console.WriteLine($"Druid - {Name} healed for {Power}");
+        }
+    }
+}
