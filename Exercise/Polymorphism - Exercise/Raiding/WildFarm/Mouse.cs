2418992af1e7723da775dad6acb3b856bd0beb59
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:19:10 +0300

    Mouse.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Mouse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Mouse : Mammal
+    {
+        public Mouse(string name, double weight, string livingRegion)
+            : base(name, weight, livingRegion)
+        {
+        }
+
+        public override void Feed(Food food)
+        {
+            if (food.GetType().Name == "Vegetable" || food.GetType().Name == "Fruit")
+            {
+                Weight += food.Quantity * 0.10;
+                FoodEaten += food.Quantity;
+            }
+        }
+
+        public override void SoundAbility()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+}
