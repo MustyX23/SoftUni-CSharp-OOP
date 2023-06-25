4d459b20f4ba2343564334e09908f22614cf77ac
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:18:41 +0300

    Feline.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Feline.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Feline : Mammal
+    {
+        public string Breed { get; set; }
+
+        protected Feline(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion)
+        {
+            Breed = breed;
+        }
+
+        public override string ToString()
+        {
+            //•	Felines - "{AnimalType} [{AnimalName}, {Breed}, {AnimalWeight}, {AnimalLivingRegion}, {FoodEaten}]"
+            return $"{GetType().Name} [{Name}, {Breed}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+}
