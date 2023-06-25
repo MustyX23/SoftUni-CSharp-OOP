2819b014636000b63e98855e298963416d42c3bf
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:18:19 +0300

    Rogue.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/Raiding/Rogue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        public Rogue(string name)
+            : base(name)
+        {
+            Power = 90;
+        }
+
+        public override void CastAbility()
+        {
+            Console.WriteLine($"Rogue - {Name} hit for {Power} damage"); 
+        }
+    }
+}
