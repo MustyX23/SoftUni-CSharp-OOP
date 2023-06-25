279120ad49e1d23d17589741693a4c985b056436
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:19:02 +0300

    Mammal.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Mammal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Mammal : Animal
+    {
+        public string LivingRegion { get; set; }
+
+        public Mammal(string name, double weight, string livingRegion)
+            : base(name, weight)
+        {
+            LivingRegion = livingRegion;
+        }
+
+        public override string ToString()
+        {
+            //"{AnimalType} [{AnimalName}, {AnimalWeight}, {AnimalLivingRegion}, {FoodEaten}]
+            return $"{GetType().Name} [{Name}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+
+    }
+}
