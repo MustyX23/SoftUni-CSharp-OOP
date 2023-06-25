0e06139a55755759a41d5f9c03f63d773536f35b
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:18:26 +0300

    Animal.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+            FoodEaten = 0;
+        }
+
+        //string Name, double Weight, int FoodEaten
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int FoodEaten { get; set; }
+
+        public abstract void SoundAbility();
+        public abstract void Feed(Food food);
+    }
+}
