9d0429f6e5fa7c736c8d2aaff0547342433a5fce
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:18:38 +0300

    Dog.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Dog : Mammal
+    {
+        public Dog(string name, double weight, string livingRegion)
+            : base(name, weight, livingRegion)
+        {
+        }
+
+        public override void Feed(Food food)
+        {
+            if (food.GetType().Name == "Meat")
+            {
+                Weight += food.Quantity * 0.40;
+                FoodEaten += food.Quantity;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+
+        public override void SoundAbility()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+}
