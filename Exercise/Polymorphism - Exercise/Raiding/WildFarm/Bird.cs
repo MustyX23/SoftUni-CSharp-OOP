09816e7d4692e189527ab6aeb888721d03bef0ab
MustyX23 <mustyx900@gmail.com>
2023-06-25 15:18:30 +0300

    Bird.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Bird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Bird : Animal
+    {
+        public Bird(string name, double weight, double wingSize) 
+            :base(name, weight)
+        {
+            WingSize = wingSize;
+        }
+
+        public double WingSize { get; set; }
+
+        //•	Birds - "{AnimalType} [{AnimalName}, {WingSize}, {AnimalWeight}, {FoodEaten}]"
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {WingSize}, {Weight}, {FoodEaten}]";
+        }
+
+    }
+}
