b1c130e3103c33e289000700c526c9c80cf6dcdc
MustyX23 <mustyx900@gmail.com>
2023-06-26 12:57:16 +0300

    Car.cs

---
+++ Exercise/Polymorphism - Exercise/Vehicles/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicle
+    {
+
+        public Car(double fuelQuantity, double fuelConsumption)
+            : base(fuelQuantity, fuelConsumption)
+        {
+            FuelConsumptionPerKm += 0.9;
+        }
+
+        public override void Drive(double distance)
+        {
+            double liters = FuelConsumptionPerKm * distance;
+                       
+            if (FuelQuantity < liters)
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+            else
+            {
+                TotalKilometres += distance;
+                FuelQuantity -= liters;
+                Console.WriteLine($"Car travelled {distance} km");
+            }
+            
+        }
+
+        public override void Refuel(double liters)
+        {
+            FuelQuantity += liters;
+        }
+        public override string ToString()
+        {
+            return $"Car: {FuelQuantity:f2}";
+        }
+    }
+}
