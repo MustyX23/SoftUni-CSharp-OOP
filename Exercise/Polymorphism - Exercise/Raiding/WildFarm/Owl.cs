ba2836ef3aeed9a2cdf2383d867f79695e4b1009
MustyX23 <mustyx900@gmail.com>
2023-06-26 12:56:48 +0300

    Owl.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Owl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Owl : Bird
+    {
+        public Owl(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+        }
+
+        public override void Feed(Food food)
+        {
+            if (food.GetType().Name == "Meat")
+            {
+                Weight += food.Quantity * 0.25;
+                FoodEaten += food.Quantity;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+
+        public override void SoundAbility()
+        {
+            Console.WriteLine("Hoot Hoot");
+        }
+    }
+}
