17a778130af95536d020d14a8193d41f81c0e217
MustyX23 <mustyx900@gmail.com>
2023-06-26 12:56:58 +0300

    Tiger.cs

---
+++ Exercise/Polymorphism - Exercise/Raiding/WildFarm/Tiger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Tiger : Feline
+    {
+        public Tiger(string name, double weight, string livingRegion, string breed) 
+            : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override void Feed(Food food)
+        {
+            if (food.GetType().Name == "Meat")
+            {
+                Weight += food.Quantity * 1.00;
+                FoodEaten += food.Quantity;
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} does not eat {food.GetType().Name}!");
+            }
+        }
+
+        public override void SoundAbility()
+        {
+            Console.WriteLine("ROAR!!!");
+        }
+    }
+}
