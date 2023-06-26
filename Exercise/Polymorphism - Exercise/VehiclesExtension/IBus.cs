b4154a555acc1e99e2091d6bde250814e189efb8
MustyX23 <mustyx900@gmail.com>
2023-06-26 12:57:50 +0300

    IBus.cs

---
+++ Exercise/Polymorphism - Exercise/VehiclesExtension/IBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesExtencion
+{
+    public interface IBus 
+    {
+        void DriveBus(double distance);
+    }
+}
