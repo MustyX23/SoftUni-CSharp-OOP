f9d8c33e569f6a61d3c520d660f3b4580df50bca
MustyX23 <mustyx900@gmail.com>
2023-06-27 21:43:40 +0300

    Team.cs

---
+++ Lab/Encapsulation - Lab/04. Team/Team.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public string Name 
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {FirstTeam.Count} players.\nReserve team has {ReserveTeam.Count} players.";
+        }
+    }
+}
