56b0f7caf26749e7178c1fffa7b38bfe4d762398
MustyX23 <mustyx900@gmail.com>
2023-06-27 21:44:18 +0300

    Program.cs

---
+++ Lab/Exception Handling/04. Sum of Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04._Sum_of_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int totalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    int currentElement = int.Parse(input[i]);
+                    totalSum += currentElement;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{input[i]}' processed - current sum: {totalSum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {totalSum}");
+
+        }
+    }
+}
