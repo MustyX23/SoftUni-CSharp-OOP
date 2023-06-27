49028294dd281b3fd07b47ba3bc9c353cf90fdd8
MustyX23 <mustyx900@gmail.com>
2023-06-27 21:43:53 +0300

    Program.cs

---
+++ Lab/Exception Handling/01. Square Root/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				double number = double.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                double rootedNumber = Math.Sqrt(number);
+				Console.WriteLine(rootedNumber);
+				
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Goodbye.");
+			}
+        }
+    }
+}
