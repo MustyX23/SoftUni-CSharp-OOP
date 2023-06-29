39c69a3dcf30cbd58580076018261fdbe1af37b9
MustyX23 <mustyx900@gmail.com>
2023-06-29 13:48:35 +0300

    Tesla.cs

---
+++ Lab/Interfaces and Abstraction - Lab/Cars/Tesla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+            :base()
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public int Battery { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries\n{Start()}\n{Stop()}";
+        }
+    }
+}
