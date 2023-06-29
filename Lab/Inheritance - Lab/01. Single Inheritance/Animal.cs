add39a345c1649ceaf5f50c31144b8ebc6618eb6
MustyX23 <mustyx900@gmail.com>
2023-06-29 13:47:11 +0300

    Animal.cs

---
+++ Lab/Inheritance - Lab/01. Single Inheritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        //Animal with a single public method Eat() that prints: "eating…"
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
