19e752786a918b0bdba28cf809c31dbf4114e3e6
MustyX23 <mustyx900@gmail.com>
2023-06-29 13:48:32 +0300

    Seat.cs

---
+++ Lab/Interfaces and Abstraction - Lab/Cars/Seat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : Car
+    {
+        public Seat(string model, string color)
+            :base()
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}\n{Start()}\n{Stop()}";
+        }
+    }
+}
