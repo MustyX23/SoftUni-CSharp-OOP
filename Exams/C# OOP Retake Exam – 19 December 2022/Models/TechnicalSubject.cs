cc55caef2ff15c80884df853cb5b6d2e6ce647e4
MustyX23 <mustyx900@gmail.com>
2023-07-01 11:55:16 +0300

    TechnicalSubject.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Models/TechnicalSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCompetition.Models
+{
+    public class TechnicalSubject : Subject
+    {
+        private const double subjectRate = 1.3;
+        public TechnicalSubject(int id, string name)
+            : base(name, subjectRate)
+        {
+        }
+    }
+}
