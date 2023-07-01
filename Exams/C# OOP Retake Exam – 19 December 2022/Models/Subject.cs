a257b096d299e7dde58f6895f86557e94c9f6e54
MustyX23 <mustyx900@gmail.com>
2023-07-01 11:55:11 +0300

    Subject.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Models/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UniversityCompetition.Models.Contracts;
+
+namespace UniversityCompetition.Models
+{
+    public abstract class Subject : ISubject
+    {
+        private int nextId = 1;
+        private string name;
+
+        protected Subject(string name, double rate)
+        {
+            Id = nextId++;
+            Name = name;
+            Rate = rate;
+        }
+
+        public int Id { get; private set; }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or whitespace!");
+                }
+                name = value;
+            }
+        }
+        public double Rate { get; private set; }
+
+
+    }
+}
