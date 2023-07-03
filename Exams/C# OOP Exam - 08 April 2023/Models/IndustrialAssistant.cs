f47f03c471b67f772e5d6e7820c1901ade349347
MustyX23 <mustyx900@gmail.com>
2023-07-04 00:30:23 +0300

    IndustrialAssistant.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/Models/IndustrialAssistant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models
+{
+    public class IndustrialAssistant : Robot
+    {
+        public IndustrialAssistant(string model)
+            : base(model, 40000, 5000)
+        {
+        }
+    }
+}
