32dfdfed637e3d89227002c683dafb9dcfd4f8d0
MustyX23 <mustyx900@gmail.com>
2023-07-04 00:30:56 +0300

    RobotRepository.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/Repositories/RobotRepository.cs
@@ -0,0 +1,48 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotService.Repositories
+{
+    public class RobotRepository : IRepository<IRobot>
+    {
+        private readonly List<IRobot> robots;
+
+        public RobotRepository()
+        {
+            robots = new List<IRobot>();
+        }
+
+        public IReadOnlyCollection<IRobot> Models()
+        {
+            return robots.AsReadOnly();
+        }
+
+        public void AddNew(IRobot robot)
+        {
+            robots.Add(robot);
+        }
+
+        public bool RemoveByName(string robotModel)
+        {
+            IRobot robot = robots.FirstOrDefault(r => r.Model == robotModel);
+
+            if (robot != null)
+            {
+                robots.Remove(robot);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IRobot FindByStandard(int interfaceStandard)
+        {
+            return robots.FirstOrDefault(r => r.InterfaceStandards.Contains(interfaceStandard));
+        }
+
+    }
+}
