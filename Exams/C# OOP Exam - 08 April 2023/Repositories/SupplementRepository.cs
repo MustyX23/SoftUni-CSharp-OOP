e933eb930197375cb5505b9727f03393af6ad28e
MustyX23 <mustyx900@gmail.com>
2023-07-04 00:31:13 +0300

    SupplementRepository.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/Repositories/SupplementRepository.cs
@@ -0,0 +1,47 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotService.Repositories
+{
+    public class SupplementRepository : IRepository<ISupplement>
+    {
+        private readonly List<ISupplement> supplements;
+
+        public SupplementRepository()
+        {
+            supplements = new List<ISupplement>();
+        }
+
+        public IReadOnlyCollection<ISupplement> Models()
+        {
+            return supplements.AsReadOnly();
+        }
+
+        public void AddNew(ISupplement supplement)
+        {
+            supplements.Add(supplement);
+        }
+
+        public bool RemoveByName(string typeName)
+        {
+            ISupplement supplement = supplements.FirstOrDefault(s => s.GetType().Name == typeName);
+
+            if (supplement != null)
+            {
+                supplements.Remove(supplement);
+                return true;
+            }
+
+            return false;
+        }
+
+        public ISupplement FindByStandard(int interfaceStandard)
+        {
+            return supplements.FirstOrDefault(s => s.InterfaceStandard == interfaceStandard);
+        }
+    }
+}
