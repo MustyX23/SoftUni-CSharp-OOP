9f61da0e7cb903778e3a89ee4b1f89396e7a31d2
MustyX23 <mustyx900@gmail.com>
2023-07-04 00:30:34 +0300

    SpecializedArm.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/Models/SpecializedArm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models
+{
+    public class SpecializedArm : Supplement
+    {
+        public SpecializedArm()
+            : base(10045, 10000)
+        {
+        }
+    }
+}
