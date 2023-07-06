872bc9bcee7e1b31e256c6b3bbc8b06be5fc5291
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:07:03 +0300

    CocktailRepository.cs

---
+++ Exams/C# OOP Exam - 10 December 2022/Repositories/CocktailRepository.cs
@@ -0,0 +1,23 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class CocktailRepository : IRepository<ICocktail>
+    {
+        private readonly List<ICocktail> cocktails;
+        public IReadOnlyCollection<ICocktail> Models => cocktails;
+
+        public CocktailRepository()
+        {
+            cocktails = new List<ICocktail>();
+        }
+        public void AddModel(ICocktail model)
+        {
+            this.cocktails.Add(model);
+        }
+    }
+}
