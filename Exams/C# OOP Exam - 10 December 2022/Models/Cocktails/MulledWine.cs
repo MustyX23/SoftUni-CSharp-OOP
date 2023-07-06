d85b044a99b5a4b2332e8ca2b6d3ecf74c6a738b
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:06:40 +0300

    MulledWine.cs

---
+++ Exams/C# OOP Exam - 10 December 2022/Models/Cocktails/MulledWine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public class MulledWine : Cocktail
+    {
+        private const double largePrice = 13.50;
+
+        public MulledWine(string cocktailName, string size)
+            : base(cocktailName, size, largePrice)
+        {
+        }
+    }
+}
