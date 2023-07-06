a7336651e860294df389593c571fae758af8f4d6
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:07:11 +0300

    DelicacyRepository.cs

---
+++ Exams/C# OOP Exam - 10 December 2022/Repositories/DelicacyRepository.cs
@@ -0,0 +1,24 @@
+﻿using ChristmasPastryShop.Models.Delicacies;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class DelicacyRepository : IRepository<IDelicacy>
+    {
+        private readonly List<IDelicacy> delicacies;
+        public IReadOnlyCollection<IDelicacy> Models => delicacies;
+
+        public DelicacyRepository()
+        {
+            delicacies = new List<IDelicacy>();
+        }
+        public void AddModel(IDelicacy model)
+        {
+            this.delicacies.Add(model);
+        }
+    }
+}
