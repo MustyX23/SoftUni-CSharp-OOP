541340d3bd3acdad49cd41de7b036ab545c9495b
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:07:57 +0300

    Student.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Models/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+
+namespace UniversityCompetition.Models
+{
+    public class Student : IStudent
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private List<int> coveredExams;
+
+        public Student(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            coveredExams = new List<int>();
+        }
+
+        public int Id
+        {
+            get => id;
+            private set => id = value; 
+        }
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or whitespace!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or whitespace!");
+                }
+                lastName = value;
+            }
+        }
+
+        public IReadOnlyCollection<int> CoveredExams => coveredExams;
+
+        public IUniversity University { get; private set; }
+
+        public void CoverExam(ISubject subject)
+        {
+            coveredExams.Add(subject.Id);
+        }
+
+        public void JoinUniversity(IUniversity university)
+        {
+            University = university;
+        }
+    }
+}
