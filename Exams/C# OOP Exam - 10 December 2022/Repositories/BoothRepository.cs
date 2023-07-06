8cc8b6e9c7fbe713f60b35d8029210fb34c5a77f
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:07:00 +0300

    BoothRepository.cs

---
+++ Exams/C# OOP Exam - 10 December 2022/Repositories/BoothRepository.cs
@@ -0,0 +1,23 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class BoothRepository : IRepository<IBooth>
+    {
+        private readonly List<IBooth> booths;
+        public IReadOnlyCollection<IBooth> Models => booths;
+
+        public BoothRepository()
+        {
+            booths = new List<IBooth>();
+        }
+        public void AddModel(IBooth model)
+        {
+            this.booths.Add(model);
+        }
+    }
+}
