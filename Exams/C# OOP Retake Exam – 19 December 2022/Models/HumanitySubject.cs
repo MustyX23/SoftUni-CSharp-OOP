e089fa04addfe3781ef3efe28d2af7d6c621d896
MustyX23 <mustyx900@gmail.com>
2023-07-06 05:07:49 +0300

    HumanitySubject.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Models/HumanitySubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCompetition.Models
+{
+    public class HumanitySubject : Subject
+    {
+        private const double subjectRate = 1.0;
+        public HumanitySubject(int id, string name)
+            : base(id, name, subjectRate)
+        {
+        }
+    }
+}
