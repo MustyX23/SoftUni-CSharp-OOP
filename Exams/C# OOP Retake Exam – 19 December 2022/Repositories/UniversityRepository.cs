342de7e4d244ad204e3d5e14c1bd3dc49b3389ff
MustyX23 <mustyx900@gmail.com>
2023-07-11 22:31:53 +0300

    UniversityRepository.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Repositories/UniversityRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class UniversityRepository : IRepository<IUniversity>
+    {
+        private List<IUniversity> universities; 
+        public IReadOnlyCollection<IUniversity> Models => universities;
+
+        public UniversityRepository()
+        {
+            this.universities = new List<IUniversity>();
+        }
+        public void AddModel(IUniversity model)
+        {
+            this.universities.Add(model);
+        }
+
+        public IUniversity FindById(int id)
+        {
+            return this.universities.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IUniversity FindByName(string name)
+        {
+            return this.universities.FirstOrDefault(u => u.Name == name);
+        }
+    }
+}
