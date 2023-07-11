fe0ffb0160bbb02cc622731cb93bc8ab7cd3a0cd
MustyX23 <mustyx900@gmail.com>
2023-07-11 22:30:45 +0300

    SubjectRepository.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/Repositories/SubjectRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class SubjectRepository : IRepository<ISubject>
+    {
+        private List<ISubject> subjects;
+
+        public IReadOnlyCollection<ISubject> Models => subjects;
+        
+
+        public SubjectRepository()
+        {
+            subjects = new List<ISubject>();
+        }
+
+        public void AddModel(ISubject model)
+        {
+            this.subjects.Add(model);
+        }
+
+        public ISubject FindById(int id)
+        {
+            return this.subjects.FirstOrDefault(s => s.Id == id);
+        }
+
+        public ISubject FindByName(string name)
+        {
+            return this.subjects.FirstOrDefault(s => s.Name == name);
+        }
+    }
+}
