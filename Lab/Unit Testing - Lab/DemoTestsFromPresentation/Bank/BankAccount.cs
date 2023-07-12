b35c2d0db45edd62198a55c39fa934b295acbe35
MustyX23 <mustyx900@gmail.com>
2023-07-12 17:09:16 +0300

    BankAccount.cs

---
+++ Lab/Unit Testing - Lab/DemoTestsFromPresentation/Bank/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public BankAccount(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public decimal Account { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
