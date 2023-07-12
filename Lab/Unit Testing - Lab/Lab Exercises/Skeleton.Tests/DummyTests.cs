5ef0168281c13a8ab8aa51fb3b936961a5812b84
MustyX23 <mustyx900@gmail.com>
2023-07-12 17:09:40 +0300

    DummyTests.cs

---
+++ Lab/Unit Testing - Lab/Lab Exercises/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealthIfAttacked()
+        {
+            Axe baltiq = new Axe(10, 10);
+            Dummy dummy = new Dummy(11, 10);
+
+            dummy.TakeAttack(1);
+
+            Assert.AreEqual(10, dummy.Health, "Dummy is not taking damage.");
+        }
+        [Test]
+        public void DeadDummyThrowsAnExceptionIfAttacked()
+        {
+            Axe baltiq = new Axe(10, 10);
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(1), "Dummy is taking damage while dead.");
+        }
+        [Test]
+        public void DeadDummyThrowsCanGiveXp()
+        {
+            Axe baltiq = new Axe(10, 10);
+            Dummy dummy = new Dummy(0, 10);
+
+            int xp = dummy.GiveExperience();
+            Assert.AreEqual(10, xp, $"Experience is not equal to the expected!");
+        }
+        [Test]
+        public void DeadDummyThrowsCannotGiveXp()
+        {
+            Axe baltiq = new Axe(10, 10);
+            Dummy dummy = new Dummy(1, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience(), "Dummy is dead, it must give XP.");
+        }
+    }
+}
