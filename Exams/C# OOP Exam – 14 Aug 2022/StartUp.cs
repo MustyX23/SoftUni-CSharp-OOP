eda2eeea8037562161f9ac06672cdcdc508cd0e5
MustyX23 <mustyx900@gmail.com>
2023-07-17 09:28:44 +0300

    StartUp.cs

---
+++ Exams/C# OOP Exam – 14 Aug 2022/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using PlanetWars.Core;
+using PlanetWars.Models.MilitaryUnits;
+using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Models.Planets;
+using PlanetWars.Models.Weapons;
+using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace PlanetWars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Don't forget to comment out the commented code lines in the Engine class!
+            var engine = new Engine();
+            engine.Run();
+
+            
+        }
+    }
+}
