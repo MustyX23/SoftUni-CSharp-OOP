13736e654cf0956c74e6de988f1818340bbfd87f
MustyX23 <mustyx900@gmail.com>
2023-07-17 09:28:35 +0300

    UnitRepository.cs

---
+++ Exams/C# OOP Exam – 14 Aug 2022/Repositories/UnitRepository.cs
@@ -0,0 +1,48 @@
+﻿using PlanetWars.Models.MilitaryUnits;
+using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private List<IMilitaryUnit> units;
+
+        public UnitRepository()
+        {
+            units = new List<IMilitaryUnit>();
+        }
+        public IReadOnlyCollection<IMilitaryUnit> Models
+        {
+            get { return units; }
+        }
+
+        public void AddItem(IMilitaryUnit model)
+        {
+            units.Add(model);
+        }
+
+        public IMilitaryUnit FindByName(string name)
+        {
+            IMilitaryUnit militaryUnit = null;
+
+            return militaryUnit = units.FirstOrDefault(u => u.GetType().Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            IMilitaryUnit militaryUnit = units.FirstOrDefault(u => u.GetType().Name == name);
+
+            if (units.Contains(militaryUnit))
+            {
+                units.Remove(militaryUnit);
+                return true;
+            }
+            return false;
+        }
+    }
+}
