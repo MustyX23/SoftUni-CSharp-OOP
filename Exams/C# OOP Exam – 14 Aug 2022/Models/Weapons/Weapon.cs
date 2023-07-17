ec16a8f66a5e488f59863ea3163ae783598e0385
MustyX23 <mustyx900@gmail.com>
2023-07-17 09:28:02 +0300

    Weapon.cs

---
+++ Exams/C# OOP Exam – 14 Aug 2022/Models/Weapons/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        private double price;
+        private int destructionLevel;
+
+        protected Weapon(int destructionLevel, double price)
+        {
+            DestructionLevel = destructionLevel;
+            Price = price;
+        }
+
+        public int DestructionLevel
+        {
+            get { return destructionLevel; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Destruction level cannot be zero or negative.");
+                }
+                if (value > 10)
+                {
+                    throw new ArgumentException("Destruction level cannot exceed 10 power points.");
+                }
+                destructionLevel = value;
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+    }
+}
