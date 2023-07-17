ec09dac288cdef1032b49f25ac95f1fc6c440336
MustyX23 <mustyx900@gmail.com>
2023-07-17 09:28:39 +0300

    WeaponRepository.cs

---
+++ Exams/C# OOP Exam – 14 Aug 2022/Repositories/WeaponRepository.cs
@@ -0,0 +1,43 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            weapons = new List<IWeapon>();
+        }
+        public IReadOnlyCollection<IWeapon> Models => weapons;
+
+        public void AddItem(IWeapon model)
+        {
+            weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            IWeapon weapon = null;
+            return weapon = weapons.FirstOrDefault(W => W.GetType().Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            IWeapon weapon = weapons.FirstOrDefault(W => W.GetType().Name == name);
+            if (weapons.Contains(weapon))
+            {
+                weapons.Remove(weapon);
+                return true;
+            }
+            return false;
+            
+        }
+    }
+}
