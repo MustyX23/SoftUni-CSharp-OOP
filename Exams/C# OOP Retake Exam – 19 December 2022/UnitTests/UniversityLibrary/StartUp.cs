6a4c649966fbdbcdc36476597166b38a9ac7367f
MustyX23 <mustyx900@gmail.com>
2023-07-18 20:52:33 +0300

    StartUp.cs

---
+++ Exams/C# OOP Retake Exam – 19 December 2022/UnitTests/UniversityLibrary/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversityLibrary
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            TextBook textBook = new TextBook("Dune", "Ivan", "Fantasy");
+            Console.WriteLine(textBook.InventoryNumber);
+            Console.WriteLine(textBook.Holder);
+        }
+    }
+}
