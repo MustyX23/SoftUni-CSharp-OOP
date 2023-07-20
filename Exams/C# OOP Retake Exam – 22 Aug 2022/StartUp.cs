aa4b1f356d6e9147c9a3b725235c5e2f3e7b3226
MustyX23 <mustyx900@gmail.com>
2023-07-20 15:32:49 +0300

    StartUp.cs

---
+++ Exams/C# OOP Retake Exam – 22 Aug 2022/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace BookingApp
+{
+    using BookingApp.Core;
+    using BookingApp.Core.Contracts;
+    using BookingApp.Models.Bookings;
+    using BookingApp.Models.Rooms;
+    using BookingApp.Models.Rooms.Contracts;
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            IEngine engine = new Engine();
+            engine.Run();
+
+        }
+    }
+}
