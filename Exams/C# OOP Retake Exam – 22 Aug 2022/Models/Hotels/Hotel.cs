1dab2e9862bfda4196c35fcb11557fa7558f0bfa
MustyX23 <mustyx900@gmail.com>
2023-07-20 15:32:09 +0300

    Hotel.cs

---
+++ Exams/C# OOP Retake Exam – 22 Aug 2022/Models/Hotels/Hotel.cs
@@ -0,0 +1,64 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        private string fullName;
+        private int category;
+        private  BookingRepository bookings;
+        private  RoomRepository rooms;
+
+        public Hotel(string fullName, int category)
+        {
+            FullName = fullName;
+            Category = category;
+            bookings = new BookingRepository();
+            rooms = new RoomRepository();
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hotel name cannot be null or empty!");
+                }
+                fullName = value;
+            }
+        }
+        public int Category
+        {
+            get { return category; }
+            private set
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentException("Category should be between 1 and 5 stars!");
+                }
+                category = value;
+            }
+        }
+        public double Turnover
+        {
+            get 
+            {
+                return Bookings.All().Sum(booking => booking.ResidenceDuration * booking.Room.PricePerNight);
+            }           
+        }
+
+        public IRepository<IRoom> Rooms => rooms;
+
+        public IRepository<IBooking> Bookings => bookings;
+    }
+}
