98baf67328e4cd3b5843703dde9f3be5e1dc9269
MustyX23 <mustyx900@gmail.com>
2023-07-21 20:38:37 +0300

    Create PilotRepository.cs

---
+++ Exams/C# OOP Exam - 09 April 2022/Repositories/PilotRepository.cs
@@ -0,0 +1,41 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private List<IPilot> pilots;
+
+        public PilotRepository()
+        {
+            pilots = new List<IPilot>();
+        }
+        public IReadOnlyCollection<IPilot> Models => pilots.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            pilots.Add(model);
+        }
+        public bool Remove(IPilot model)
+        {
+            IPilot pilot = pilots.FirstOrDefault(pilot => pilot == model);
+            if (pilot != null)
+            {
+                pilots.Remove(pilot);
+                return true;
+            }
+            return false;   
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return pilots.FirstOrDefault(p => p.FullName == name);
+        }
+
+    }
+}
