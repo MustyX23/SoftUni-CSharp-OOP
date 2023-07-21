3cfdcd762bc18cfc6673b7afddec6807736787b5
MustyX23 <mustyx900@gmail.com>
2023-07-21 20:38:34 +0300

    Create FormulaOneCarRepository.cs

---
+++ Exams/C# OOP Exam - 09 April 2022/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,43 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Formula1.Repositories
+{
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private List<IFormulaOneCar> formulaOneCars;
+
+        public FormulaOneCarRepository()
+        {
+            formulaOneCars = new List<IFormulaOneCar>();
+        }
+        public IReadOnlyCollection<IFormulaOneCar> Models => formulaOneCars.AsReadOnly();
+
+        public void Add(IFormulaOneCar model)
+        {
+            formulaOneCars.Add(model);
+        }
+        public bool Remove(IFormulaOneCar model)
+        {
+            IFormulaOneCar car = formulaOneCars.FirstOrDefault(f => f == model);
+            if (car != null)
+            {
+                formulaOneCars.Remove(car);
+                return true;
+            }
+            return false;
+
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            return formulaOneCars.FirstOrDefault(fcar => fcar.Model == name);
+        }
+
+    }
+}
