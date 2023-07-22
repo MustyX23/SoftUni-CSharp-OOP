3bf52effe09f7bfe08d7b4c9fd030b81c49e9152
MustyX23 <mustyx900@gmail.com>
2023-07-22 16:20:57 +0300

    Create Mace.cs

---
+++ Exams/C# OOP Retake Exam - 18 April 2022/First & Second Task/Heroes/Models/Weapons/Mace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        public Mace(string name, int durability)
+            : base(name, durability)
+        {
+        }
+
+        public override int DoDamage()
+        {
+            if (Durability > 0)
+            {
+                DecreaseDurability();
+                return 25;
+            }
+            return 0;
+        }
+    }
+}
