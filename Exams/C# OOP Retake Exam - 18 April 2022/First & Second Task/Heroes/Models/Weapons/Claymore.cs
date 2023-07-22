14916a93347f381941093f30c777ef90e7716382
MustyX23 <mustyx900@gmail.com>
2023-07-22 16:20:55 +0300

    Create Claymore.cs

---
+++ Exams/C# OOP Retake Exam - 18 April 2022/First & Second Task/Heroes/Models/Weapons/Claymore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        public Claymore(string name, int durability)
+            : base(name, durability)
+        {
+
+        }
+
+        public override int DoDamage()
+        {
+            if (Durability > 0)
+            {
+                DecreaseDurability();
+                return 20;
+            }
+            return 0;
+        }
+    }
+}
