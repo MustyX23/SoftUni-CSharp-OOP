4a7f019c6c3a4332f1910fb2a5d8e32d014612cb
MustyX23 <mustyx900@gmail.com>
2023-07-22 16:20:59 +0300

    Create Weapon.cs

---
+++ Exams/C# OOP Retake Exam - 18 April 2022/First & Second Task/Heroes/Models/Weapons/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        private string name;
+        private int durability;
+
+        protected Weapon(string name, int durability)
+        {
+            Name = name;
+            Durability = durability;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.WeaponTypeNull);
+                }
+                name = value;
+            }
+        }
+        public int Durability 
+        {
+            get => durability;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.DurabilityBelowZero);
+                }
+                durability = value;
+            }
+        }
+
+        public abstract int DoDamage();
+        protected void DecreaseDurability()
+        {
+            Durability--;
+        }
+    }
+}
