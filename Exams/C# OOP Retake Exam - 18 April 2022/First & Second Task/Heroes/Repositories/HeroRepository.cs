4571c25780ae27502983abe0d3a148f5542db559
MustyX23 <mustyx900@gmail.com>
2023-07-22 16:21:04 +0300

    Create HeroRepository.cs

---
+++ Exams/C# OOP Retake Exam - 18 April 2022/First & Second Task/Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,39 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class HeroRepository : IRepository<IHero>
+    {
+        private readonly List<IHero> heroes;
+        public HeroRepository()
+        {
+            heroes = new List<IHero>();
+        }
+             
+        public void Add(IHero model)
+        {
+            if (!heroes.Any(h => h.Name == model.Name))
+            {
+                heroes.Add(model);
+            }
+        }
+
+        public bool Remove(IHero model)
+        {
+            return heroes.Remove(model);
+        }
+
+        public IHero FindByName(string name)
+        {
+            return heroes.FirstOrDefault(h => h.Name == name);
+        }
+
+        public IReadOnlyCollection<IHero> Models => heroes.AsReadOnly();
+
+    }
+}
