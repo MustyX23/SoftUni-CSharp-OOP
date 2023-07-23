2e9871fcc22ef1b27d214e38a3d931bb239dfa06
MustyX23 <mustyx900@gmail.com>
2023-07-23 16:24:17 +0300

    Create User.cs

---
+++ Exams/C# OOP Retake Exam – 18 April 2023/First & Second/Models/User.cs
@@ -0,0 +1,99 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private double rating;
+        private string drivingLicenseNumber;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DrivingLicenseNumber = drivingLicenseNumber;
+            Rating = 0;
+            IsBlocked = false;
+        }
+
+        public string FirstName 
+        {
+            get => firstName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.FirstNameNull);
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.LastNameNull);
+                }
+                lastName = value;
+            }
+        }
+
+
+        public string DrivingLicenseNumber
+        {
+            get => drivingLicenseNumber;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.DrivingLicenseRequired);
+                }
+                drivingLicenseNumber = value;
+            }
+        }
+        public double Rating 
+        {
+            get => rating;
+            private set
+            {
+                rating = value;
+            }
+        }
+
+        public bool IsBlocked { get; private set; }
+        public void IncreaseRating()
+        {
+            Rating += 0.5;
+            if (Rating > 10)
+            {
+                Rating = 10;
+            }
+        }
+
+        public void DecreaseRating()
+        {
+            Rating -= 2;
+            if (Rating < 0.0)
+            {
+                Rating = 0.0;
+                IsBlocked = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} Driving license: {DrivingLicenseNumber} Rating: {Rating}";
+        }
+
+    }
+}
