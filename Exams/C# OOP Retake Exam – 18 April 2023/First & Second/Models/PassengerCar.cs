7e278e5d2e3be91e5dc0e377fc9072027672d9a8
MustyX23 <mustyx900@gmail.com>
2023-07-23 16:24:07 +0300

    Create PassengerCar.cs

---
+++ Exams/C# OOP Retake Exam – 18 April 2023/First & Second/Models/PassengerCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDriveRent.Models
+{
+    public class PassengerCar : Vehicle
+    {
+        private const double MaxMileagePassengerCar = 450;
+        public PassengerCar(string brand, string model, string licensePlateNumber)
+            : base(brand, model, MaxMileagePassengerCar, licensePlateNumber)
+        {
+        }
+    }
+}
