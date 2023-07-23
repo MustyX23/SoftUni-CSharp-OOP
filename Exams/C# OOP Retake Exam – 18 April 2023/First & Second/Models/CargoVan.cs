e46647fe5bdc7a6e9be4fb339cc976bc313fa905
MustyX23 <mustyx900@gmail.com>
2023-07-23 16:23:57 +0300

    Create CargoVan.cs

---
+++ Exams/C# OOP Retake Exam – 18 April 2023/First & Second/Models/CargoVan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDriveRent.Models
+{
+    public class CargoVan : Vehicle
+    {
+        private const double MaxMileageCargoVan = 180;
+        public CargoVan(string brand, string model, string licensePlateNumber) 
+            : base(brand, model, MaxMileageCargoVan, licensePlateNumber)
+        {
+        }
+    }
+}
