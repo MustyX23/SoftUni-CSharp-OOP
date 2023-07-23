6dfc78bc9785395303d1de3c413db2505ea8fea8
MustyX23 <mustyx900@gmail.com>
2023-07-23 16:24:24 +0300

    Create VehicleRepository.cs

---
+++ Exams/C# OOP Retake Exam – 18 April 2023/First & Second/Repositories/VehicleRepository.cs
@@ -0,0 +1,45 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDriveRent.Repositories
+{
+    public class VehicleRepository : IRepository<IVehicle>
+    {
+        private List<IVehicle> vehicles;
+
+        public VehicleRepository()
+        {
+            vehicles = new List<IVehicle>();
+        }
+
+        public void AddModel(IVehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        public bool RemoveById(string identifier)
+        {
+            IVehicle vehicle = vehicles.FirstOrDefault(v => v.LicensePlateNumber == identifier);
+            if (vehicle != null)
+            {
+                vehicles.Remove(vehicle);
+                return true;
+            }
+            return false;
+        }
+
+        public IVehicle FindById(string identifier)
+        {
+            return vehicles.FirstOrDefault(v => v.LicensePlateNumber == identifier);
+        }
+
+        public IReadOnlyCollection<IVehicle> GetAll()
+        {
+            return vehicles.AsReadOnly();
+        }
+    }
+}
