a74cf44a739625af553d80431121172fab0b6efc
MustyX23 <mustyx900@gmail.com>
2023-07-23 16:24:23 +0300

    Create UserRepository.cs

---
+++ Exams/C# OOP Retake Exam – 18 April 2023/First & Second/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDriveRent.Repositories
+{
+    public class UserRepository : IRepository<IUser>
+    {
+        private List<IUser> users;
+
+        public UserRepository()
+        {
+            users = new List<IUser>();
+        }
+
+        public void AddModel(IUser user)
+        {
+            users.Add(user);
+        }
+
+        public bool RemoveById(string identifier)
+        {
+            IUser user = users.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+            if (user != null)
+            {
+                users.Remove(user);
+                return true;
+            }
+            return false;
+        }
+
+        public IUser FindById(string identifier)
+        {
+            return users.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+        }
+
+        public IReadOnlyCollection<IUser> GetAll()
+        {
+            return users.AsReadOnly();
+        }
+    }
+}
