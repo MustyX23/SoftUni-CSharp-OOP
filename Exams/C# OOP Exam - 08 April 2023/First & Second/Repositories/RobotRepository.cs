9903deee3f0f8dc7824624aa0d86fed60613d2a7
MustyX23 <mustyx900@gmail.com>
2023-07-25 23:16:44 +0300

    Create RobotRepository.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/First & Second/Repositories/RobotRepository.cs
@@ -0,0 +1,43 @@
+﻿using RobotService.Models;
+using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotService.Repositories
+{
+    public class RobotRepository : IRepository<IRobot>
+    {
+        private List<IRobot> robots;
+
+        public RobotRepository()
+        {
+            robots = new List<IRobot>();
+        }
+        public void AddNew(IRobot model)
+        {
+            robots.Add(model);
+        }
+        public bool RemoveByName(string typeName)
+        {
+            IRobot robot = robots.FirstOrDefault(r => r.GetType().Name == typeName);
+
+            if (robot != null)
+            {
+                robots.Remove(robot);
+                return true;
+            }
+            return false;
+        }
+
+        public IRobot FindByStandard(int interfaceStandard)
+        {
+            return robots.FirstOrDefault(r => r.InterfaceStandards.Contains(interfaceStandard));
+        }
+
+        public IReadOnlyCollection<IRobot> Models() => robots.AsReadOnly();
+        
+    }
+}
