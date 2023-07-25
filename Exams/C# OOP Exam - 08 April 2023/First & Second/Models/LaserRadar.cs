45114287606701a5d2a122cd93742f4496da77c1
MustyX23 <mustyx900@gmail.com>
2023-07-25 23:16:36 +0300

    Create LaserRadar.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/First & Second/Models/LaserRadar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models
+{
+    public class LaserRadar : Supplement
+    {
+        public LaserRadar()
+            : base(20082, 5000)
+        {
+        }
+    }
+}
