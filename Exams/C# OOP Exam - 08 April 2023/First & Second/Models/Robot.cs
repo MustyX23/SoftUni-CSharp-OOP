9181e40366662d493e28f890b4a3552289f76dda
MustyX23 <mustyx900@gmail.com>
2023-07-25 23:16:38 +0300

    Create Robot.cs

---
+++ Exams/C# OOP Exam - 08 April 2023/First & Second/Models/Robot.cs
@@ -0,0 +1,115 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models
+{
+    public abstract class Robot : IRobot
+    {
+        private string model;
+        private int batteryCapacity;
+        private int batteryLevel;
+        private List<int> interfaceStandards;
+
+        protected Robot(string model, int batteryCapacity, int convertionCapacityIndex)
+        {
+            Model = model;
+            BatteryCapacity = batteryCapacity;
+            BatteryLevel = BatteryCapacity;
+            ConvertionCapacityIndex = convertionCapacityIndex;
+            interfaceStandards = new List<int>();
+        }
+
+        public string Model 
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ModelNullOrWhitespace);
+                }
+                model = value;
+            }
+        }
+
+        public int BatteryCapacity 
+        {
+            get => batteryCapacity;
+            private set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.BatteryCapacityBelowZero);
+                }
+                batteryCapacity = value;
+            }
+        }
+        public int ConvertionCapacityIndex { get; private set; }
+
+        public int BatteryLevel
+        {
+            get => batteryLevel;
+            private set
+            {
+                batteryLevel = value;
+            }
+        }
+
+
+        public IReadOnlyCollection<int> InterfaceStandards => interfaceStandards.AsReadOnly();
+
+        public void Eating(int minutes)
+        {
+            int energyProduced = ConvertionCapacityIndex * minutes;
+            int remainingEnergy = BatteryCapacity - BatteryLevel;
+
+            if (remainingEnergy <= 0)
+            {
+                return; 
+            }
+
+            if (energyProduced >= remainingEnergy)
+            {
+                BatteryLevel = BatteryCapacity;
+            }
+            else
+            {
+                BatteryLevel += energyProduced;
+            }
+
+        }
+
+        public void InstallSupplement(ISupplement supplement)
+        {
+            interfaceStandards.Add(supplement.InterfaceStandard);
+            BatteryCapacity -= supplement.BatteryUsage;
+            batteryLevel -= supplement.BatteryUsage;
+        }
+        public bool ExecuteService(int consumedEnergy)
+        {
+            if (BatteryLevel >= consumedEnergy)
+            {
+                BatteryLevel -= consumedEnergy;
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name} {model}:");
+            sb.AppendLine($"--Maximum battery capacity: {BatteryCapacity}");
+            sb.AppendLine($"--Current battery level: {BatteryLevel}");
+
+            string standards = interfaceStandards.Count > 0 ? string.Join(" ", interfaceStandards) : "none";
+            sb.AppendLine($"--Supplements installed: {standards}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
