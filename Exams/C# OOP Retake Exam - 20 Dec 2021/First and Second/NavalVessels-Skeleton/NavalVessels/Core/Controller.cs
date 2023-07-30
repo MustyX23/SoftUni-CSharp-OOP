c49683bbb8a675244321baa9d90c7bc6ef6549c8
MustyX23 <mustyx900@gmail.com>
2023-07-30 21:19:44 +0300

    Create Controller.cs

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/Core/Controller.cs
@@ -0,0 +1,157 @@
+﻿using NavalVessels.Core.Contracts;
+using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System.Collections.Generic;
+using System;
+using NavalVessels.Models;
+using NavalVessels.Repositories;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace NavalVessels.Core
+{
+    public class Controller : IController
+    {
+        private IRepository<IVessel> vessels;
+        private List<ICaptain> captains;
+
+        public Controller()
+        {
+            vessels = new VesselRepository();
+            captains = new List<ICaptain>();
+        }
+
+        public string HireCaptain(string fullName)
+        {
+            ICaptain captain = new Captain(fullName);
+            if (captains.Any(c => c.FullName == fullName))
+            {
+                return $"Captain {fullName} is already hired.";
+            }
+
+            captains.Add(captain);
+            return $"Captain {fullName} is hired.";
+        }
+        public string ProduceVessel(string name, string vesselType, double mainWeaponCaliber, double speed)
+        {
+            if (vesselType != nameof(Battleship) && vesselType != nameof(Submarine))
+            {
+                return "Invalid vessel type.";
+            }
+
+            IVessel vessel = vessels.FindByName(name);
+            if (vessel != null)
+            {
+                return $"{vesselType} vessel {name} is already manufactured.";
+            }
+
+            if (vesselType == nameof(Battleship))
+            {
+                vessel = new Battleship(name, mainWeaponCaliber, speed);
+            }
+            else if (vesselType == nameof(Submarine))
+            {
+                vessel = new Submarine(name, mainWeaponCaliber, speed);
+            }
+
+            vessels.Add(vessel);
+            return $"{vesselType} {name} is manufactured with the main weapon caliber of {mainWeaponCaliber} inches and a maximum speed of {speed} knots.";
+        }
+        public string AssignCaptain(string selectedCaptainName, string selectedVesselName)
+        {
+            ICaptain captain = captains.FirstOrDefault(c => c.FullName == selectedCaptainName);
+            IVessel vessel = vessels.FindByName(selectedVesselName);
+
+            if (captain == null)
+            {
+                return $"Captain {selectedCaptainName} could not be found.";
+            }
+            if (vessel == null)
+            {
+                return $"Vessel {selectedVesselName} could not be found.";
+            }
+
+            if (captain.Vessels.Any(v => v.Name == selectedVesselName))
+            {
+                return $"Vessel {selectedVesselName} is already occupied.";
+            }
+
+            captain.AddVessel(vessel);
+            vessel.Captain = captain;
+            return $"Captain {selectedCaptainName} command vessel {selectedVesselName}.";
+        }
+        public string CaptainReport(string captainFullName)
+        {
+            ICaptain captain = captains.FirstOrDefault(c => c.FullName == captainFullName);
+            return captain.Report();
+        }
+        public string VesselReport(string vesselName)
+        {
+            IVessel vessel = vessels.FindByName(vesselName);
+            return vessel.ToString();
+        }
+        public string ToggleSpecialMode(string vesselName)
+        {
+            IVessel vessel = vessels.FindByName(vesselName);
+
+            if (vessel == null)
+            {
+                return $"Vessel {vesselName} could not be found.";
+            }
+            if (vessel is Battleship battleship)
+            {
+                battleship.ToggleSonarMode();
+                return $"Battleship {vesselName} toggled sonar mode.";
+            }
+            if (vessel is Submarine submarine)
+            {
+                submarine.ToggleSubmergeMode();
+                return $"Submarine {vesselName} toggled submerge mode.";
+            }
+
+            return $"Vessel {vesselName} could not be found.";
+        }
+
+        public string ServiceVessel(string vesselName)
+        {
+            IVessel vessel = vessels.FindByName(vesselName);
+
+            if (vessel == null)
+            {
+                return $"Vessel {vesselName} could not be found.";
+            }
+                
+            vessel.RepairVessel();
+            return $"Vessel {vesselName} was repaired.";
+        }
+        public string AttackVessels(string attackingVesselName, string defendingVesselName)
+        {
+            IVessel attackingVessel = vessels.FindByName(attackingVesselName);
+            IVessel defendingVessel = vessels.FindByName(defendingVesselName);
+
+            if (attackingVessel == null)
+            {
+                return $"Vessel {attackingVesselName} could not be found.";
+            }
+            if (defendingVessel == null)
+            {
+                return $"Vessel {defendingVesselName} could not be found.";
+            }
+
+            if (attackingVessel.ArmorThickness == 0)
+            {
+                return $"Unarmored vessel {attackingVesselName} cannot attack or be attacked.";
+            }
+            if (defendingVessel.ArmorThickness == 0)
+            {
+                return $"Unarmored vessel {defendingVesselName} cannot attack or be attacked.";
+            }
+
+            attackingVessel.Attack(defendingVessel);
+            attackingVessel.Captain.IncreaseCombatExperience();
+            defendingVessel.Captain.IncreaseCombatExperience();
+
+            return $"Vessel {defendingVesselName} was attacked by vessel {attackingVesselName} - current armor thickness: {defendingVessel.ArmorThickness}.";
+        }                      
+    }
+}
