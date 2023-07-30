6c41460ae6003159a6d5ad1191f34e9eeadc6c90
MustyX23 <mustyx900@gmail.com>
2023-07-30 21:19:56 +0300

    Create Battleship.cs

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/Models/Battleship.cs
@@ -0,0 +1,59 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Battleship : Vessel, IBattleship
+    {
+        private const double BattleShipArmorThickness = 300;
+        public Battleship(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, BattleShipArmorThickness)
+        {
+            this.SonarMode = false;
+        }
+
+        public bool SonarMode { get; private set; }
+
+        public override void RepairVessel()
+        {
+            if (this.ArmorThickness < BattleShipArmorThickness)
+            {
+                this.ArmorThickness = BattleShipArmorThickness;
+            }                        
+        }
+
+        public void ToggleSonarMode()
+        {
+            if (SonarMode == false)
+            {
+                this.SonarMode = true;
+                this.MainWeaponCaliber += 40;
+                this.Speed -= 5;
+            }
+            else
+            {
+                this.SonarMode = false;
+                this.MainWeaponCaliber -= 40;
+                this.Speed += 5;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            if (this.SonarMode == true)
+            {
+                sb.AppendLine(" *Sonar mode: ON");
+            }
+            else
+            {
+                sb.AppendLine(" *Sonar mode: OFF");
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
