09b6f1844adb659be912b6d586e469c3a89dec8b
MustyX23 <mustyx900@gmail.com>
2023-07-30 21:19:58 +0300

    Create Captain.cs

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/Models/Captain.cs
@@ -0,0 +1,65 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Captain : ICaptain
+    {
+        private string fullName;
+        private ICollection<IVessel> vessels;
+
+        public Captain(string fullName)
+        {
+            FullName = fullName;
+            vessels = new List<IVessel>();
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Captain full name cannot be null or empty string.");
+                }
+                fullName = value;
+            }
+        }
+
+        public int CombatExperience { get; private set; }
+
+        public ICollection<IVessel> Vessels => vessels;
+
+        public void AddVessel(IVessel vessel)
+        {
+            if (vessel == null)
+            {
+                throw new NullReferenceException("Null vessel cannot be added to the captain.");
+            }
+            vessels.Add(vessel);
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            this.CombatExperience += 10;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{FullName} has {CombatExperience} combat experience and commands {this.vessels.Count} vessels.");
+            if (this.vessels.Count > 0)
+            {
+                foreach (var vessel in vessels)
+                {
+                    sb.AppendLine(vessel.ToString());
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
