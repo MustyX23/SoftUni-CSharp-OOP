33a20258ae35efa5a93c1d90797ae9bd2969f6f3
MustyX23 <mustyx900@gmail.com>
2023-08-02 18:41:54 +0300

    Create Component.cs

---
+++ Exams/C# OOP Exam - 16 August 2020/First and Second/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Components
+{
+    public abstract class Component : Product, IComponent
+    {
+        protected Component(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            Generation = generation;
+        }
+
+        public int Generation { get; private set; }
+
+        public override string ToString()
+        {
+            string productType = this.GetType().Name;
+            return $" Overall Performance: {OverallPerformance:F2}. Price: {Price:F2} - {productType}: {Manufacturer} {Model} (Id: {Id}) Generation: {Generation}";
+        }
+    }
+}
