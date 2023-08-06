413f10fa0f99f157b08f3d8ae8519fbcf9c80dd7
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:27 +0300

    Create Student.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Clients/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Clients
+{
+    public class Student : Client
+    {
+        //Has an initial interest of 2 percent.
+        public Student(string name, string id, double income)
+            : base(name, id, 2, income)
+        {
+        }
+
+        public override void IncreaseInterest()
+        {
+            Interest += 1;
+        }
+    }
+}
