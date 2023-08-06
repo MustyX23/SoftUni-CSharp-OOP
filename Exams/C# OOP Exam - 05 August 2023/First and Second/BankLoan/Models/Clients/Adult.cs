1a4c41e5d09db0fda4ec8350bb959d4553973782
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:24 +0300

    Create Adult.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Clients/Adult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Clients
+{
+    public class Adult : Client
+    {
+        //Has an initial interest of 4 percent.
+
+        public Adult(string name, string id, double income)
+            : base(name, id, 4, income)
+        {
+
+        }
+
+        public override void IncreaseInterest()
+        {
+            Interest += 2;
+        }
+    }
+}
