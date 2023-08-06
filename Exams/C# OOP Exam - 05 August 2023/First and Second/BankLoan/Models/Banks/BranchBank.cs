0a4acc618b6d82eaf713d94eee05cd8f6f6807cc
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:20 +0300

    Create BranchBank.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Banks/BranchBank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Banks
+{
+    public class BranchBank : Bank
+    {
+        public BranchBank(string name)
+            : base(name, 25)
+        {
+        }
+    }
+}
