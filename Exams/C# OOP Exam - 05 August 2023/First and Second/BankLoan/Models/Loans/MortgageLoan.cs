612cf95fb9315a54fe4fe721d318f824bdc44ba1
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:35 +0300

    Create MortgageLoan.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Loans/MortgageLoan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Loans
+{
+    public class MortgageLoan : Loan
+    {
+        public MortgageLoan()
+            : base(3, 50000)
+        {
+        }
+    }
+}
