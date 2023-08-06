6c7e7ac6843e07cfd8a597e3a54668fb9f85311f
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:43 +0300

    Create LoanRepository.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Repositories/LoanRepository.cs
@@ -0,0 +1,36 @@
+﻿using BankLoan.Models;
+using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankLoan.Repositories
+{
+    public class LoanRepository : IRepository<ILoan>
+    {
+        private readonly List<ILoan> loans;
+
+        public LoanRepository()
+        {
+            loans = new List<ILoan>();
+        }
+        public IReadOnlyCollection<ILoan> Models => loans.AsReadOnly();
+        public bool RemoveModel(ILoan model)
+        {
+            return loans.Remove(model);
+        }
+
+        public void AddModel(ILoan model)
+        {
+            loans.Add(model);
+        }
+
+        public ILoan FirstModel(string name)
+        {
+            return loans.FirstOrDefault(loan => loan.GetType().Name == name);
+        }
+
+    }
+}
