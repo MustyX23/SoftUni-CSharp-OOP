716d4b277b36358ce727b5b61172de64be421600
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:26 +0300

    Create Client.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Clients/Client.cs
@@ -0,0 +1,66 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Clients
+{
+    public abstract class Client : IClient
+    {
+        private string name;
+        private string id;
+        private double income;
+
+        protected Client(string name, string id, int interest, double income)
+        {
+            Name = name;
+            Id = id;
+            Interest = interest;
+            Income = income;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ClientNameNullOrWhitespace);
+                }
+                name = value;
+            }
+        }
+
+        public string Id
+        {
+            get => id;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ClientIdNullOrWhitespace);
+                }
+                id = value;
+            }
+        }
+
+        public int Interest { get; protected set; } //Be careful with the access modifier.
+
+        public double Income
+        {
+            get => income;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.ClientIncomeBelowZero);
+                }
+                income = value;
+            }
+        }
+
+        public abstract void IncreaseInterest();
+    }
+}
