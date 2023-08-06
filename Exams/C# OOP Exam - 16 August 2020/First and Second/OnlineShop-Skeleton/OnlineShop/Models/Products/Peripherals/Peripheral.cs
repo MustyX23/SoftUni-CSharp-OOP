6aab2118e74d296ffcfad9ec323dd58e8895c723
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:30:31 +0300

    Create Peripheral.cs

---
+++ Exams/C# OOP Exam - 16 August 2020/First and Second/OnlineShop-Skeleton/OnlineShop/Models/Products/Peripherals/Peripheral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Peripherals
+{
+    public abstract class Peripheral : Product, IPeripheral
+    {
+        protected Peripheral(int id, string manufacturer, string model, decimal price, double overallPerformance, string connectionType)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            ConnectionType = connectionType;
+        }
+
+        public string ConnectionType { get; private set; }
+
+        public override string ToString()
+        {
+            string productType = this.GetType().Name;
+            return $" Overall Performance: {OverallPerformance:F2}. Price: {Price:F2} - {productType}: {Manufacturer} {Model} (Id: {Id}) Connection Type: {ConnectionType}";
+        }
+
+    }
+}
