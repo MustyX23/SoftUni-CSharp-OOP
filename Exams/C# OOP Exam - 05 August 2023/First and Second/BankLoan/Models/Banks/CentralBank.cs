c65043897901fbcb477cd13f4e250518a35d64e9
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:22 +0300

    Create CentralBank.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Banks/CentralBank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Banks
+{
+    public class CentralBank : Bank
+    {
+        public CentralBank(string name)
+            : base(name, 50)
+        {
+        }
+    }
+}
