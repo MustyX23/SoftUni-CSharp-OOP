b96bbf4d701b5b4b2f05d08fba5073714fe7cb5c
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:38 +0300

    Create BankRepository.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Repositories/BankRepository.cs
@@ -0,0 +1,37 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankLoan.Repositories
+{
+    public class BankRepository : IRepository<IBank>
+    {
+        //Ela tuka smeni HashSeta ako neshto
+        private readonly HashSet<IBank> banks;
+
+        public BankRepository()
+        {
+            banks = new HashSet<IBank>();
+        }
+
+        public IReadOnlyCollection<IBank> Models => banks;
+
+        public void AddModel(IBank model)
+        {
+            banks.Add(model);
+        }
+        public bool RemoveModel(IBank model)
+        {
+            return banks.Remove(model);
+        }
+
+        public IBank FirstModel(string name)
+        {
+            return banks.FirstOrDefault(bank => bank.Name == name);
+        }
+
+    }
+}
