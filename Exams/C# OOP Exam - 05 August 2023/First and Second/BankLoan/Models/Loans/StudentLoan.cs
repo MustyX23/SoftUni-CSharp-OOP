7bbac32589293932bd3ae1caaa5197d38c822483
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:37 +0300

    Create StudentLoan.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Loans/StudentLoan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Loans
+{
+    public class StudentLoan : Loan
+    {
+        public StudentLoan()
+            : base(1, 10000)
+        {
+        }
+    }
+}
