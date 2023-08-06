3ec22cc68ddacb1eb8a6b542ea865a32969e535d
MustyX23 <mustyx900@gmail.com>
2023-08-06 14:29:33 +0300

    Create Loan.cs

---
+++ Exams/C# OOP Exam - 05 August 2023/First and Second/BankLoan/Models/Loans/Loan.cs
@@ -0,0 +1,20 @@
+﻿using BankLoan.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Models.Loans
+{
+    public abstract class Loan : ILoan
+    {
+        protected Loan(int interestRate, double amount)
+        {
+            InterestRate = interestRate;
+            Amount = amount;
+        }
+
+        public int InterestRate { get; private set; }
+
+        public double Amount { get; private set; }
+    }
+}
