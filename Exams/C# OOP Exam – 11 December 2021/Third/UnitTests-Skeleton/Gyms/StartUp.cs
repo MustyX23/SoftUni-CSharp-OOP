83cb64abcaadfbd5302eafbc222d59a0f591faa6
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:33 +0300

    Create StartUp.cs

---
+++ Exams/C# OOP Exam – 11 December 2021/Third/UnitTests-Skeleton/Gyms/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Gyms
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Gym gym = new Gym("Rich", 3);
+            Athlete athlete = new Athlete("Messi");
+
+            gym.AddAthlete(athlete);
+
+            gym.InjureAthlete("Messi");
+
+            Console.WriteLine(gym.Report());
+        }
+    }
+}
