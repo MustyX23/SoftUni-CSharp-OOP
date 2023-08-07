32f6a99060273a013d07c7070fec6e645cb70e3e
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:15:20 +0300

    Create Vessel.cs

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/Models/Vessel.cs
@@ -0,0 +1,99 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public abstract class Vessel : IVessel
+    {
+        private string name;
+        private ICaptain captain;
+        private ICollection<string> targets;
+
+        public Vessel(string name, double mainWeaponCaliber, double speed, double armorThickness)
+        {
+            Name = name;
+            MainWeaponCaliber = mainWeaponCaliber;
+            Speed = speed;
+            ArmorThickness = armorThickness;
+            targets = new List<string>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Name), ExceptionMessages.InvalidVesselName);
+                }
+                name = value;
+            }
+        }
+        public ICaptain Captain
+        {
+            get => captain;
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(ExceptionMessages.InvalidCaptainName);
+                }
+                captain = value;
+            }
+        }
+
+        public double ArmorThickness { get; set; }
+
+        public double MainWeaponCaliber { get; protected set; }
+
+        public double Speed { get; protected set; }
+
+        public ICollection<string> Targets => targets;
+
+        public void Attack(IVessel target)
+        {
+            if (target == null)
+            {
+                throw new NullReferenceException("Target cannot be null.");
+            }
+
+            double damage = MainWeaponCaliber;
+
+            target.ArmorThickness -= damage;
+
+            if (target.ArmorThickness < 0)
+            {
+                target.ArmorThickness = 0;
+            }
+
+            targets.Add(target.Name);
+        }
+
+        public abstract void RepairVessel(); 
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"- {this.Name}");
+            sb.AppendLine($" *Type: {this.GetType().Name}");
+            sb.AppendLine($" *Armor thickness: {(int)Math.Round(this.ArmorThickness)}");
+            sb.AppendLine($" *Main weapon caliber: {this.MainWeaponCaliber}");
+            sb.AppendLine($" *Speed: {this.Speed} knots");
+            if (targets.Count == 0)
+            {
+                sb.AppendLine($" *Targets: None");
+            }
+            else
+            {
+                sb.Append($" *Targets: ");
+                sb.Append(String.Join(", ", Targets));
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
