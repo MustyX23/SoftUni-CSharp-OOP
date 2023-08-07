b122acbec05b40ff8af79e1078e3f9ec3541607f
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:13:49 +0300

    Create Boxer.cs

---
+++ Exams/C# OOP Exam – 11 December 2021/First and Second/Skeleton/Gym/Models/Athletes/Boxer.cs
@@ -0,0 +1,28 @@
+﻿using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    public class Boxer : Athlete
+    {
+        private const int InititalBoxerStamina = 60;
+        public Boxer(string fullName, string motivation, int numberOfMedals)
+            : base(fullName, motivation, numberOfMedals, InititalBoxerStamina)
+        {
+        }
+
+        //Can train only in a BoxingGym.
+
+        public override void Exercise()
+        {
+            this.Stamina += 15;
+            if (this.Stamina > 100)
+            {
+                Stamina = 100;
+                throw new ArgumentException(ExceptionMessages.InvalidStamina);
+            }
+        }
+    }
+}
