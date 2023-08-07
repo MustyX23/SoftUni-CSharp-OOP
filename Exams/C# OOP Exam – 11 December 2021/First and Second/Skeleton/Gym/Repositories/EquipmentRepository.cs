714106c1b465b7ad087dbd74ab6f3b772c7bfe69
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:10 +0300

    Create EquipmentRepository.cs

---
+++ Exams/C# OOP Exam – 11 December 2021/First and Second/Skeleton/Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,41 @@
+﻿using Gym.Models.Athletes;
+using Gym.Models.Equipment.Contracts;
+using Gym.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Repositories
+{
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private readonly List<IEquipment> equipments;
+
+        public EquipmentRepository()
+        {
+            equipments = new List<IEquipment>();
+        }
+        public IReadOnlyCollection<IEquipment> Models => equipments.AsReadOnly();
+
+        public void Add(IEquipment equipment)
+        {
+            equipments.Add(equipment);
+        }
+
+        public IEquipment FindByType(string equipmentType)
+        {
+            return equipments.FirstOrDefault(e => e.GetType().Name == equipmentType);
+        }
+
+        public bool Remove(IEquipment equipment)
+        {
+            if (equipments.Contains(equipment))
+            {
+                equipments.Remove(equipment);
+                return true;
+            }
+            return false;
+        }
+    }
+}
