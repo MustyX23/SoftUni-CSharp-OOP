589edb814037a945458f7b2f36debf7a362ee27e
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:40 +0300

    Update BioChemicalWeapon.cs

--- Exams/C# OOP Exam – 14 Aug 2022/Models/Weapons/BioChemicalWeapon.cs
+++ Exams/C# OOP Exam – 14 Aug 2022/Models/Weapons/BioChemicalWeapon.cs
@@ -6,8 +6,9 @@ namespace PlanetWars.Models.Weapons
 {
     public class BioChemicalWeapon : Weapon
     {
+        private const double BioChemicalWeaponPrice = 3.2;
         public BioChemicalWeapon(int destructionLevel)
-            : base(destructionLevel, 3.2)
+            : base(destructionLevel, BioChemicalWeaponPrice)
         {
         }
     }
