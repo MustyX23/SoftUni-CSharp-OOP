17596f9ac4086e4d58e68d048a95ee90d393d1e6
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:15:34 +0300

    Create VesselRepository.cs

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/Repositories/VesselRepository.cs
@@ -0,0 +1,34 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NavalVessels.Repositories
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        private readonly ICollection<IVessel> vessels;
+
+        public VesselRepository()
+        {
+            vessels = new HashSet<IVessel>();
+        }
+        public IReadOnlyCollection<IVessel> Models => (IReadOnlyCollection<IVessel>)vessels;
+
+        public void Add(IVessel model)
+        {
+            vessels.Add(model);
+        }
+
+        public IVessel FindByName(string name)
+        {
+            return vessels.FirstOrDefault(v => v.Name == name);
+        }
+
+        public bool Remove(IVessel model)
+        {
+            IVessel vessel = FindByName(model.Name);
+            return vessels.Remove(vessel);
+        }
+    }
+}
