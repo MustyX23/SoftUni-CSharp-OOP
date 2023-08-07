6e1706fe92df47308f1448235994210aae382fee
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:43 +0300

    Update SpaceMissiles.cs

--- Exams/C# OOP Exam – 14 Aug 2022/Models/Weapons/SpaceMissiles.cs
+++ Exams/C# OOP Exam – 14 Aug 2022/Models/Weapons/SpaceMissiles.cs
@@ -6,8 +6,9 @@ namespace PlanetWars.Models.Weapons
 {
     public class SpaceMissiles : Weapon
     {
+        private const double SpaceMissilesPrice = 8.75;
         public SpaceMissiles(int destructionLevel)
-            : base(destructionLevel, 8.75)
+            : base(destructionLevel, SpaceMissilesPrice)
         {
         }
     }
