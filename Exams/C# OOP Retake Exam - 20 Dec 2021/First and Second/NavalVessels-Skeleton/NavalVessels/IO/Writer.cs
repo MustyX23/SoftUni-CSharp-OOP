9c70f4ec3626473a0fca974fba9479768170aac2
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:58 +0300

    Update Writer.cs

--- Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/IO/Writer.cs
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/First and Second/NavalVessels-Skeleton/NavalVessels/IO/Writer.cs
@@ -7,13 +7,15 @@
     public class Writer : IWriter
     {
         public void Write(string message)
-        {
+        {            
             Console.Write(message);
         }
 
         public void WriteLine(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
