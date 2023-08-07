f96ca4eb5f934446313c87e2aba803a54f57a54f
MustyX23 <mustyx900@gmail.com>
2023-08-07 21:14:01 +0300

    Create Kettlebell.cs

---
+++ Exams/C# OOP Exam – 11 December 2021/First and Second/Skeleton/Gym/Models/Equipment/Kettlebell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    public class Kettlebell : Equipment
+    {
+        private const double KettlebellWeight = 10000;
+        private const decimal KettlebellPrice = 80m;
+
+        public Kettlebell()
+            : base(KettlebellWeight, KettlebellPrice)
+        {
+        }
+    }
+}
