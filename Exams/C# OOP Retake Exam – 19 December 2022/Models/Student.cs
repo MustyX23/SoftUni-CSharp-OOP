09a42b54fd510fd363c08ed2fcde44b6b3866025
MustyX23 <mustyx900@gmail.com>
2023-08-08 22:51:12 +0300

    Update Student.cs

--- Exams/C# OOP Retake Exam – 19 December 2022/Models/Student.cs
+++ Exams/C# OOP Retake Exam – 19 December 2022/Models/Student.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using UniversityCompetition.Models.Contracts;
 
@@ -8,39 +7,35 @@ namespace UniversityCompetition.Models
 {
     public class Student : IStudent
     {
-        private int id;
+        private int studentId;
         private string firstName;
         private string lastName;
-        private List<int> coveredExams;
 
-        public Student(int id, string firstName, string lastName)
-        {
-            Id = id;
-            FirstName = firstName;
-            LastName = lastName;
-            coveredExams = new List<int>();
-        }
+        private List<int> coveredExams; // za sega e taka
 
         public int Id
         {
-            get => id;
-            private set => id = value; 
-        }
-        public string FirstName
-        {
-            get => firstName;
+            get { return studentId; }
             private set
+            {
+                studentId = value;
+            }
+        }
+        public string FirstName 
+        {
+            get { return firstName; }
+            private set 
             {
                 if (string.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentException("Name cannot be null or whitespace!");
                 }
-                firstName = value;
+                firstName = value; 
             }
         }
         public string LastName
         {
-            get => lastName;
+            get { return lastName; }
             private set
             {
                 if (string.IsNullOrWhiteSpace(value))
@@ -50,11 +45,19 @@ namespace UniversityCompetition.Models
                 lastName = value;
             }
         }
-
-        public IReadOnlyCollection<int> CoveredExams => coveredExams;
-
         public IUniversity University { get; private set; }
 
+        
+
+        public Student(int studentId, string firstName, string lastName)
+        {
+            Id = studentId;
+            FirstName = firstName;
+            LastName = lastName;
+            coveredExams = new List<int>();
+        }
+
+        public IReadOnlyCollection<int> CoveredExams => coveredExams.AsReadOnly();
         public void CoverExam(ISubject subject)
         {
             coveredExams.Add(subject.Id);
