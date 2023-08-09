03aede27f0304da4f32c306b5d1d785bb8ab7239
MustyX23 <mustyx900@gmail.com>
2023-08-09 10:45:17 +0300

    Create IWarrior.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/DemoTest/HeroContracts/IWarrior.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.DemoTest.HeroContracts
+{
+    public interface IWarrior
+    {
+        void Attack();
+    }
+}
