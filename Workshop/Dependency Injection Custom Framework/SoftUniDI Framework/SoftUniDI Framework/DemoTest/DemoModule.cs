842873942aaf780ed21eba2734acf1942450c2fa
MustyX23 <mustyx900@gmail.com>
2023-08-09 10:45:15 +0300

    Create DemoModule.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/DemoTest/DemoModule.cs
@@ -0,0 +1,18 @@
+﻿using SoftUniDI_Framework.DemoTest.HeroContracts;
+using SoftUniDI_Framework.DemoTest.HeroModels;
+using SoftUniDI_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.DemoTest
+{
+    public class DemoModule : AbstractModule
+    {
+        public override void Configure()
+        {
+            CreateMapping<IWeapon, Sword>();
+            CreateMapping<IWarrior, Swordsmanship>();
+        }
+    }
+}
