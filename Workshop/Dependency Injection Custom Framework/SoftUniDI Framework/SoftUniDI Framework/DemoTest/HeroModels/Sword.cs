407aa0cefce1fe1e68645d3b9aac0b358b1f474c
MustyX23 <mustyx900@gmail.com>
2023-08-09 10:45:22 +0300

    Create Sword.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/DemoTest/HeroModels/Sword.cs
@@ -0,0 +1,15 @@
+﻿using SoftUniDI_Framework.DemoTest.HeroContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.DemoTest.HeroModels
+{
+    public class Sword : IWeapon
+    {
+        public void Use()
+        {
+            Console.WriteLine("⚔︎ Swish! ▬▬ι═══════ﺤ Swish! ⚔︎");
+        }
+    }
+}
