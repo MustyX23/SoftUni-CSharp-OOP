75ad5881e67c3139eba070496ce1cd0530d796e1
MustyX23 <mustyx900@gmail.com>
2023-08-10 17:51:37 +0300

    Create FoodAsterisk.cs

---
+++ Workshop/SnakeGame/SimpleSnake/GameObjects/Food/FoodAsterisk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class FoodAsterisk : Food
+    {
+        private const char Asterisk = '*';
+        private const int Points = 1;
+
+        public FoodAsterisk(Wall wall)
+            : base(wall, Asterisk, Points)
+        {
+        }
+    }
+}
