e82c0bdb2797fdeb2e10eb29cbdc4f8000aa0b6d
MustyX23 <mustyx900@gmail.com>
2023-08-10 17:51:27 +0300

    Create StartUp.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SoftUniDI_Framework.DemoTest;
+using SoftUniDI_Framework.DemoTest.HeroContracts;
+using SoftUniDI_Framework.Models;
+using System;
+
+namespace SoftUniDI_Framework
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Dependency Injector Framework :)");
+
+            var module = new DemoModule();
+
+            var injector = DependencyInjector.CreateInjector(module);
+
+            var swordsMan = injector.Inject<IWarrior>();
+            
+
+            // Output: The swordsMan attacks with Swinging the sword!
+        }
+
+    }
+}
