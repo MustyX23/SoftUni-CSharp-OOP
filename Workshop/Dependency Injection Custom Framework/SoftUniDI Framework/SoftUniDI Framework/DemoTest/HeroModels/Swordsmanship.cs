3ef8be5f3b52dd842e6dfa68bd91c409bb0c5df2
MustyX23 <mustyx900@gmail.com>
2023-08-10 17:51:13 +0300

    Create Swordsmanship.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/DemoTest/HeroModels/Swordsmanship.cs
@@ -0,0 +1,27 @@
+﻿using SoftUniDI_Framework.DemoTest.HeroContracts;
+using SoftUniDI_Framework.Models.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.DemoTest.HeroModels
+{
+    public class Swordsmanship : IWarrior
+    {
+        private readonly IWeapon weapon;
+
+        [Inject]
+        public Swordsmanship(IWeapon weapon, string name)
+        {
+            weapon = new Sword();
+            Name = name;
+        }
+        public string Name { get; private set; }
+
+        public void Attack()
+        {
+            Console.Write("The Swordsmanship attacks with ");
+            weapon.Use();
+        }
+    }
+}
