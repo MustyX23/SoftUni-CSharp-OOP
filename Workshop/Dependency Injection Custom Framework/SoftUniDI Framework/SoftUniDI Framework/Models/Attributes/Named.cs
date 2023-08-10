3a82b5bf105d54d59677b8b1be09476409993fb8
MustyX23 <mustyx900@gmail.com>
2023-08-10 17:51:18 +0300

    Create Named.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/Models/Attributes/Named.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field)]
+    public class Named : Attribute
+    {
+        public Named(string name)
+        {
+            Name = name;    
+        }
+        public string Name { get; private set; }
+    }
+}
