068658bc5a33e8d2ef6aad738f230bbdbafe3bbe
MustyX23 <mustyx900@gmail.com>
2023-08-10 17:51:20 +0300

    Create DependencyInjector.cs

---
+++ Workshop/Dependency Injection Custom Framework/SoftUniDI Framework/SoftUniDI Framework/Models/DependencyInjector.cs
@@ -0,0 +1,16 @@
+﻿using SoftUniDI_Framework.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.Models
+{
+    public class DependencyInjector
+    {
+        public static Injector CreateInjector(IModule module)
+        {
+            module.Configure();
+            return new Injector(module);
+        }
+    }
+}
