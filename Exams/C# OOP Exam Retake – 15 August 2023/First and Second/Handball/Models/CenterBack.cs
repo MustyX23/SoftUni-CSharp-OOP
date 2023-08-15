bd0acfaf14b3a62080e83b1bf9c7b46f98e5f083
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:08:06 +0300

    Create CenterBack.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Models/CenterBack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class CenterBack : Player
+    {
+        public CenterBack(string name) 
+            : base(name, 4)
+        {
+        }
+
+        public override void DecreaseRating()
+        {
+            if (Rating - 1 >= 1)
+            {
+                Rating -= 1;
+            }
+            else
+            {
+                Rating = 1;
+            }
+        }
+
+        public override void IncreaseRating()
+        {
+            if (Rating + 1 <= 10)
+            {
+                Rating += 1;
+            }
+            else
+            {
+                Rating = 10;
+            }
+        }
+    }
+}
