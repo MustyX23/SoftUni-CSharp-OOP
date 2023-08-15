aeb38a2b7e4998ac0fffe981be08b65641b78930
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:08:15 +0300

    Create Goalkeeper.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Models/Goalkeeper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class Goalkeeper : Player
+    {
+        public Goalkeeper(string name)
+            : base(name, 2.5)
+        {
+        }
+        public override void IncreaseRating()
+        {
+            if (Rating + 0.75 <= 10)
+            {
+                Rating += 0.75;
+            }
+            else
+            {
+                Rating = 10;
+            }
+        }
+
+        public override void DecreaseRating()
+        {
+            if (Rating - 1.25 >= 1)
+            {
+                Rating -= 1.25;
+            }
+            else
+            {
+                Rating = 1;
+            }
+        }
+
+    }
+}
