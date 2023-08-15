b35036300ca0f61d98de0ace9a75b88ef88a1c5c
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:09:01 +0300

    Create PlayerRepository.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Repositories/PlayerRepository.cs
@@ -0,0 +1,48 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            players = new List<IPlayer>();
+        }
+
+        public IReadOnlyCollection<IPlayer> Models => players.AsReadOnly();
+
+        public void AddModel(IPlayer model)
+        {
+            players.Add(model);
+        }
+        public bool RemoveModel(string name)
+        {
+            var playerToRemove = players.FirstOrDefault(p => p.Name == name);
+            if (playerToRemove != null)
+            {
+                players.Remove(playerToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public bool ExistsModel(string name)
+        {
+            return players.Any(p => p.Name == name);
+        }
+
+        public IPlayer GetModel(string name)
+        {
+            return players.FirstOrDefault(p => p.Name == name);
+        }
+
+    }
+}
