685c6af34db6f5f62814b5cb2112ac77031526df
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:09:02 +0300

    Create TeamRepository.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Repositories/TeamRepository.cs
@@ -0,0 +1,48 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class TeamRepository : IRepository<ITeam>
+    {
+        private List<ITeam> teams;
+
+        public TeamRepository()
+        {
+            teams = new List<ITeam>();
+        }
+
+        public IReadOnlyCollection<ITeam> Models => teams.AsReadOnly();
+
+        public void AddModel(ITeam model)
+        {
+            teams.Add(model);
+        }
+
+        public bool ExistsModel(string name)
+        {
+            return teams.Any(t => t.Name == name);
+        }
+
+        public ITeam GetModel(string name)
+        {
+            return teams.FirstOrDefault(t => t.Name == name);
+        }
+
+        public bool RemoveModel(string name)
+        {
+            var teamToRemove = teams.FirstOrDefault(t => t.Name == name);
+            if (teamToRemove != null)
+            {
+                teams.Remove(teamToRemove);
+                return true;
+            }
+            return false;
+        }
+    }
+}
