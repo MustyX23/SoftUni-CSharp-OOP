91ce03b3fe808418fec8a792ebad3f2ae79860eb
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:08:13 +0300

    Create ForwardWing.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Models/ForwardWing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public class ForwardWing : Player
+    {
+        public ForwardWing(string name)
+            : base(name, 5.5)
+        {
+        }
+        public override void IncreaseRating()
+        {
+            if (Rating + 1.25 <= 10)
+            {
+                Rating += 1.25;
+            }
+            else
+            {
+                Rating = 10;
+            }
+        }
+
+        public override void DecreaseRating()
+        {
+            if (Rating - 0.75 >= 1)
+            {
+                Rating -= 0.75;
+            }
+            else
+            {
+                Rating = 1;
+            }
+        }
+
+    }
+}
