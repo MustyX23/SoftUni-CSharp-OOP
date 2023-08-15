565faaf362eb227378d7eb539b38723f9ec8510a
MustyX23 <mustyx900@gmail.com>
2023-08-16 00:08:52 +0300

    Create Player.cs

---
+++ Exams/C# OOP Exam Retake – 15 August 2023/First and Second/Handball/Models/Player.cs
@@ -0,0 +1,55 @@
+﻿using Handball.Models.Contracts;
+using Handball.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Models
+{
+    public abstract class Player : IPlayer
+    {
+        private string name;
+
+        protected Player(string name, double rating)
+        {
+            Name = name;
+            Rating = rating;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.PlayerNameNull);
+                }
+                name = value;
+            }
+        }
+
+        public double Rating { get; protected set; }
+
+        public string Team { get; private set; }
+
+        public void JoinTeam(string name)
+        {
+            Team = name;
+        }
+
+        public abstract void DecreaseRating();
+        public abstract void IncreaseRating();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}: {Name}");
+            sb.AppendLine($"--Rating: {Rating}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
